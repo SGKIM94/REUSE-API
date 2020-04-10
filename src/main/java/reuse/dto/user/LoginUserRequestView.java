8c1fa3157f893d06e66184f7866678ef35b7351b
sanggu <tkdrn8578@naver.com>
2020-04-10 18:04:07 +0900

    [MODIFY] 로그인 시 socialTokenId 만 받아서 로그인하도록 DTO 수정

--- src/main/java/reuse/dto/user/LoginUserRequestView.java
+++ src/main/java/reuse/dto/user/LoginUserRequestView.java
@@ -1,22 +1,19 @@
 package reuse.dto.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginUserRequestView {
-    private String email;
+    private String socialTokenId;
     private String password;
 
     public LoginUserRequestView() {
     }
 
-    public LoginUserRequestView(String email, String password) {
-        this.email = email;
+    public LoginUserRequestView(String socialTokenId, String password) {
+        this.socialTokenId = socialTokenId;
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
     }
 }
