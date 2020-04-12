7fd72766ca97ac9c0ea08d855fa4454bb969d0e5
sanggu <tkdrn8578@naver.com>
2020-04-12 13:14:42 +0900

    [MODIFY] 사용자가 oAuth 로 로그인 시 별도의 socialTokenId 와 socialType 으로 로그인함에 따른 불필요한 값 제거

--- src/main/java/reuse/domain/User.java
+++ src/main/java/reuse/domain/User.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,57 +18,30 @@ public class User {
     @Size(min = 1, max = 40)
     private String socialTokenId;
 
-    @Email
-    private String email;
-
     @Size(min = 1, max = 20)
     private String name;
 
-    private String password;
+    @Size(min = 1, max = 20)
+    private String socialType;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<Favorite> favorites = new ArrayList<>();
 
     @Builder
-    public User(Long id, String socialTokenId, String email, String name, String password, List<Favorite> favorites) {
+    public User(Long id, String socialTokenId, String name, String socialType, List<Favorite> favorites) {
         this.id = id;
         this.socialTokenId = socialTokenId;
-        this.email = email;
         this.name = name;
-        this.password = password;
+        this.socialType = socialType;
         this.favorites = favorites;
     }
 
-    public User(Long id, String socialTokenId, String email, String name, String password) {
-        this(id, socialTokenId, email, name, password, new ArrayList<>());
-    }
-
-    public User(Long id, String email, String name) {
-        this(id, email, name, null, new ArrayList<>());
-    }
-
-    public static User of(Long id, String socialTokenId, String email, String name, String password) {
-        return new User(null, socialTokenId, email, name, password, new ArrayList<>());
-    }
-
-
     public Long getId() {
         return id;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public List<Favorite> getFavorites() {
-        return favorites;
-    }
 }
