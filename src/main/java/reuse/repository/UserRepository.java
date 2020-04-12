d0298cf53a769a282f3ab9ac9af9582285bd1859
sanggu <tkdrn8578@naver.com>
2020-04-12 13:15:50 +0900

    [MODIFY] UserRepository 에 socialTokenId 로 검색할 수 있기 위한 method 추가

--- src/main/java/reuse/repository/UserRepository.java
+++ src/main/java/reuse/repository/UserRepository.java
@@ -2,8 +2,8 @@ package reuse.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import reuse.domain.User;
-import reuse.dto.user.FindByEmailResponseView;
+import reuse.dto.user.FindBySocialTokenIdResponseView;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    FindByEmailResponseView findByEmail(String email);
+    FindBySocialTokenIdResponseView findBySocialTokenId(String socialTokenId);
 }
