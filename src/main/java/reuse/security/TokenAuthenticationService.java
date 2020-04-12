ea326bbc2e95ebc9aec6a664aca366936e60428a
sanggu <tkdrn8578@naver.com>
2020-04-12 13:17:00 +0900

    [MODIFY] token 발급 시 email 이 아닌 socialTokenId 로 jwt 를 생성하기 위해 로직 수정

--- src/main/java/reuse/security/TokenAuthenticationService.java
+++ src/main/java/reuse/security/TokenAuthenticationService.java
@@ -13,18 +13,18 @@ public class TokenAuthenticationService {
     private static final String SALT = "63B75D39E3F6BFE72263F7C1145AC22E";
     public static final String BEARER_TOKEN_TYPE = "Bearer";
     private static final String TOKEN_TYPE_KEY = "tokenType";
-    private static final String EMAIL_KEY = "email";
+    private static final String SOCIAL_TOKEN_ID = "socialTokenId";
 
 
     public byte[] generateKey(String salt) {
         return salt.getBytes();
     }
 
-    public String toJwtByEmail(String email) {
+    public String toJwtBySocialTokenId(String socialTokenId) {
         return Jwts.builder()
-                .claim(EMAIL_KEY, email)
+                .claim(SOCIAL_TOKEN_ID, socialTokenId)
                 .setHeaderParam(TOKEN_TYPE_KEY, BEARER_TOKEN_TYPE)
-                .setSubject(email)
+                .setSubject(socialTokenId)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .signWith(SignatureAlgorithm.HS256, this.generateKey(SALT))
                 .compact();
@@ -32,7 +32,7 @@ public class TokenAuthenticationService {
 
     public String getEmailByJwt(String jwt) {
         Jws<Claims> claims =  getJwtClaim(jwt);
-        return getEmailByClaims(claims);
+        return getSocialTokenIdByClaims(claims);
     }
 
 
@@ -47,8 +47,8 @@ public class TokenAuthenticationService {
                 .isSigned(jwt);
     }
 
-    public String getEmailByClaims(Jws<Claims> claims) {
-        return claims.getBody().get(EMAIL_KEY).toString();
+    public String getSocialTokenIdByClaims(Jws<Claims> claims) {
+        return claims.getBody().get(SOCIAL_TOKEN_ID).toString();
     }
 
     public String getTokenTypeByJwt(String jwt) {
