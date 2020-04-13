e54ddf621deae3e37cf96d3d399e1d2901bc440d
sanggu <tkdrn8578@naver.com>
2020-04-13 23:05:42 +0900

    [MODIFY] toDtoWithJwt 에 대한 테스트코드 추가

--- src/test/java/reuse/service/UserServiceTest.java
+++ src/test/java/reuse/service/UserServiceTest.java
@@ -36,36 +36,15 @@ public class UserServiceTest {
         this.userService = new UserService(tokenAuthenticationService, userRepository);
     }
 
-    @DisplayName("이미 회원이 있는 유저인 경우 예외를 처리하는지")
-    @Test
-    public void sighUpWhenExistUser() {
-        when(userRepository.findByEmail(any())).thenReturn(FIND_BY_EMAIL_RESPONSE_VIEW);
-
-        Assertions.assertThrows(ExistUserException.class, () -> {
-            userService.singUp(USER_SIGH_UP_REQUEST_DTO);
-        });
-    }
-
-    @DisplayName("회원이 정상적으로 등록되는지")
-    @Test
-    public void signUp() {
-        when(userRepository.findByEmail(any())).thenReturn(FindByEmailResponseView.builder().build());
-        when(userRepository.save(any())).thenReturn(TEST_USER);
-
-        CreateUserResponseView user = userService.singUp(USER_SIGH_UP_REQUEST_DTO);
-
-        assertThat(user.getId()).isNotNull();
-    }
-
     @DisplayName("회원이 로그인이 성공하여 토큰을 리턴하는지")
     @Test
     public void login(SoftAssertions softly) {
-        when(userRepository.findByEmail(any())).thenReturn(FIND_BY_EMAIL_RESPONSE_VIEW);
+        when(userRepository.findBySocialTokenId(any())).thenReturn(FIND_BY_SOCIAL_TOKEN_ID_RESPONSE_VIEW);
 
         LoginUserResponseView user = userService.login(USER_LOGIN_REQUEST_DTO);
 
-        softly.assertThat(user.getAccessToken()).isNotNull();
-        softly.assertThat(user.getTokenType()).isEqualTo(BEARER_TOKEN_TYPE);
+        softly.assertThat(user.getSocialTokenId()).isNotNull();
+        softly.assertThat(user.getSocialType()).isEqualTo(NAVER_SOCIAL_TYPE);
     }
 
     @DisplayName("회원의 ID 로 상세정보가 되는지")
@@ -79,4 +58,12 @@ public class UserServiceTest {
         softly.assertThat(user.getName()).isEqualTo(KIM_NAME);
     }
 
+    @DisplayName("JWT 를 만들어 LoginUserResponseView 에 담아 만드는지")
+    @Test
+    public void toDtoWithJwt(SoftAssertions softly) {
+        LoginUserResponseView loginUserResponseView = userService.toDtoWithJWt(SOCIAL_TOKEN_ID);
+
+        softly.assertThat(loginUserResponseView.getSocialTokenId()).isEqualTo(SOCIAL_TOKEN_ID);
+        softly.assertThat(loginUserResponseView.getSocialType()).isEqualTo(NAVER_SOCIAL_TYPE);
+    }
 }
