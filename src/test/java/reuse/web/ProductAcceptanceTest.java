49bee3ce20abd6122b915053b10017d027eb4d67
sanggu <tkdrn8578@naver.com>
2020-04-15 22:56:49 +0900

    [MODIFY] 품목을 관리하기 위한 API 의 acceptance test 추가

---
+++ src/test/java/reuse/web/ProductAcceptanceTest.java
@@ -0,0 +1,48 @@
+package reuse.web;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import reuse.AbstractAcceptanceTest;
+import reuse.domain.User;
+import reuse.security.TokenAuthenticationService;
+
+import static reuse.fixture.UserFixture.*;
+
+public class ProductAcceptanceTest extends AbstractAcceptanceTest {
+    public static final String KIM_INPUT_JSON = "{\"email\":\"" + KIM_EMAIL + "\",\"password\":\"" + KIM_PASSWORD + "\",\"name\":\"" + KIM_NAME + "\"}";;
+    public static final String PRODUCT_BASE_URL = "/products";
+
+    private CreateWebClientTest restWebClientTest;
+    private TokenAuthenticationService tokenAuthenticationService;
+
+    @BeforeEach
+    void setUp() {
+        cleanAllDatabases();
+        this.restWebClientTest = new CreateWebClientTest(this.webTestClient);
+        this.tokenAuthenticationService = new TokenAuthenticationService();
+    }
+
+    @DisplayName("품목을 추가가 가능한지")
+    @Test
+    public void createProduct(SoftAssertions softly) {
+        //when
+        EntityExchangeResult<User> expectResponse
+                = restWebClientTest.postMethodAcceptance(PRODUCT_BASE_URL, KIM_INPUT_JSON, User.class);
+
+        //then
+        HttpHeaders responseHeaders = expectResponse.getResponseHeaders();
+        User responseBody = expectResponse.getResponseBody();
+
+        //then
+        softly.assertThat(responseHeaders.getLocation()).isNotNull();
+        softly.assertThat(responseBody.getName()).isEqualTo(KIM_NAME);
+    }
+
+    private String getJwt() {
+        return tokenAuthenticationService.toJwtBySocialTokenId(KIM_EMAIL);
+    }
+}
