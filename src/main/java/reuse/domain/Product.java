3bfe3c8506f49199812ba71b45c8b7612d625cc1
sanggu <tkdrn8578@naver.com>
2020-04-18 14:27:03 +0900

    [ADD] Product domain 의 column 에 포함되는 entity 추가
     * name(별칭), explanation(설명), price(가격), tax(세금),
     registerDate, updateDate, iUsed(중고 유무), isSold(판매 유무), sellId(판매자 외래키),
     categoryId(카테고리 외래키)

--- src/main/java/reuse/domain/Product.java
+++ src/main/java/reuse/domain/Product.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -14,6 +16,25 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(min = 1, max = 20)
+    private String name;
+
+    @Size(min = 1, max = 200)
+    private String explanation;
+
+    private String price;
+
+    private String tax;
+
+    private LocalDateTime registerDate;
+    private LocalDateTime updateDate;
+
+    private Boolean isUsed;
+    private Boolean isSold;
+
+    private String sellId;
+    private String categoryId;
 
     @Builder
     public Product(Long id) {
