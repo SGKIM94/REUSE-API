640d9ed398b9fcefdec621c446e5dfb788906fd4
sanggu <tkdrn8578@naver.com>
2020-04-20 23:19:00 +0900

    [ADD] board test 를 위한 fixture 추가

---
+++ src/test/java/reuse/fixture/BoardFixture.java
@@ -0,0 +1,21 @@
+package reuse.fixture;
+
+import reuse.domain.Board;
+import reuse.dto.board.CreateBoardRequestView;
+
+import static reuse.fixture.ProductFixture.TEST_PRODUCT;
+
+public class BoardFixture {
+    public static final String TEST_BOARD_CONTENT = "테스트 판매";
+    public static final String TEST_SELLER_ADDRESS = "경기도 성남시";
+    public static final String TEST_BOARD_TITLE = "테스트 판매 게시판";
+
+    public static final Board TEST_BOARD
+            = Board.builder().title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
+            .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
+
+
+    public static final CreateBoardRequestView CREATE_BOARD_REQUEST_VIEW
+            = CreateBoardRequestView.builder().id(1L).title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
+            .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
+}
