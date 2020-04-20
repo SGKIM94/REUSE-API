a05829df41bb6763421c59501e6e1c7c3620198c
sanggu <tkdrn8578@naver.com>
2020-04-20 23:18:36 +0900

    [ADD] 게시글 생성에 대한 test service code 추가

---
+++ src/test/java/reuse/service/BoardServiceTest.java
@@ -0,0 +1,46 @@
+package reuse.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reuse.dto.board.CreateBoardResponseView;
+import reuse.repository.BoardRepository;
+import reuse.security.TokenAuthenticationService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static reuse.fixture.BoardFixture.*;
+
+@SpringBootTest
+public class BoardServiceTest {
+    private BoardService boardService;
+
+    @MockBean
+    private BoardRepository boardRepository;
+
+    @MockBean
+    private TokenAuthenticationService tokenAuthenticationService;
+
+    @BeforeEach
+    void setUp() {
+        this.tokenAuthenticationService = new TokenAuthenticationService();
+        this.boardService = new BoardService(boardRepository);
+    }
+
+    @DisplayName("게시물이 생성되는지")
+    @Test
+    public void create() {
+        when(boardRepository.save(any())).thenReturn(TEST_BOARD);
+
+        CreateBoardResponseView board = boardService.create(CREATE_BOARD_REQUEST_VIEW);
+
+        assertThat(board.getId()).isNotNull();
+        assertThat(board.getTitle()).isEqualTo(TEST_BOARD_TITLE);
+        assertThat(board.getContent()).isEqualTo(TEST_BOARD_CONTENT);
+        assertThat(board.getProduct()).isNotNull();
+        assertThat(board.getSellerAddress()).isEqualTo(TEST_SELLER_ADDRESS);
+    }
+}
