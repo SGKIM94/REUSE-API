2a75d15c05a6a2485c4161235ac020d87bedbc6c
sanggu <tkdrn8578@naver.com>
2020-04-20 23:18:49 +0900

    [ADD] 게시판 생성을 위한 create service code 추가

---
+++ src/main/java/reuse/service/BoardService.java
@@ -0,0 +1,21 @@
+package reuse.service;
+
+import org.springframework.stereotype.Service;
+import reuse.domain.Board;
+import reuse.dto.board.CreateBoardRequestView;
+import reuse.dto.board.CreateBoardResponseView;
+import reuse.repository.BoardRepository;
+
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public CreateBoardResponseView create(CreateBoardRequestView board) {
+        Board savedBoard = boardRepository.save(CreateBoardRequestView.toEntity(board));
+        return CreateBoardResponseView.toDto(savedBoard);
+    }
+}
