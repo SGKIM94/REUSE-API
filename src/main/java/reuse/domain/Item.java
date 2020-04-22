7c1ee543592781ec2e1052dfb14a95a680868e5e
sanggu <tkdrn8578@naver.com>
2020-04-22 23:36:24 +0900

    [MODIFY] Item 에 @Entity 및 @Id 추가

--- src/main/java/reuse/domain/Item.java
+++ src/main/java/reuse/domain/Item.java
@@ -2,11 +2,16 @@ package reuse.domain;
 
 import lombok.NoArgsConstructor;
 
-import javax.persistence.OneToOne;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 @NoArgsConstructor
 public class Item {
-    @OneToOne
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Item(Long id) {
