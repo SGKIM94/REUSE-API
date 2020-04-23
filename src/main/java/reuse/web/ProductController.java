e0eea3214b1f14fd40c57779b1b35fc5641b51c1
sanggu <tkdrn8578@naver.com>
2020-04-23 23:28:29 +0900

    [MODIFY] ProductController 추가

--- src/main/java/reuse/web/ProductController.java
+++ src/main/java/reuse/web/ProductController.java
@@ -5,14 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reuse.domain.User;
-import reuse.security.LoginUser;
+import reuse.domain.Product;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     @PostMapping
-    public ResponseEntity list(@LoginUser User user) {
+    public ResponseEntity list(Product product) {
         return ResponseEntity.ok().build();
     }
 }
