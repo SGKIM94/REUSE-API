9e40884a3fd5b33348f6e09145b718550098fe11
sanggu <tkdrn8578@naver.com>
2020-04-25 16:36:39 +0900

    [ADD] Product 를 생성하기 위한 request, response dto 추가

---
+++ src/main/java/reuse/dto/product/CreateProductResponseView.java
@@ -0,0 +1,35 @@
+package reuse.dto.product;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import reuse.domain.Product;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateProductResponseView {
+    private String name;
+    private String explanation;
+    private String price;
+    private String tax;
+    private Boolean isUsed;
+    private Boolean isSold;
+    private String categoryId;
+    private String createAt;
+    private String updateAt;
+
+    @Builder
+    public CreateProductResponseView(Product product) {
+        this.name = product.getName();
+        this.explanation = product.getExplanation();
+        this.price = product.getPrice();
+        this.tax = product.getTax();
+        this.isUsed = product.getIsUsed();
+        this.isSold = product.getIsSold();
+        this.categoryId = product.getCategoryId();
+        this.createAt = product.getFormattedCreateDate();
+        this.updateAt = product.getFormattedModifyDate();
+    }
+}
