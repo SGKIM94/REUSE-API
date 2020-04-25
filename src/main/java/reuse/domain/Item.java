d56be28271385e57368c3d4fa4e4f99894f13482
sanggu <tkdrn8578@naver.com>
2020-04-25 16:35:34 +0900

    [MODIFY] Product 및 Item 의 AbstractEntity 추가로 인한 id, creatAt, updateAt 제거 및 로직 수정

--- src/main/java/reuse/domain/Item.java
+++ src/main/java/reuse/domain/Item.java
@@ -3,22 +3,11 @@ package reuse.domain;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
-public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class Item extends AbstractEntity {
     public Item(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
+        super(id);
     }
 }
--- src/main/java/reuse/domain/Product.java
+++ src/main/java/reuse/domain/Product.java
@@ -1,22 +1,16 @@
 package reuse.domain;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+@Getter
+public class Product extends AbstractEntity {
     @Size(min = 1, max = 20)
     private String name;
 
@@ -27,29 +21,22 @@ public class Product {
 
     private String tax;
 
-    private LocalDateTime registerDate;
-    private LocalDateTime updateDate;
-
     private Boolean isUsed;
+
     private Boolean isSold;
 
     private String categoryId;
 
     @Builder
-    public Product(String name, String explanation, String price, String tax, LocalDateTime registerDate,
-                   LocalDateTime updateDate, Boolean isUsed, Boolean isSold, String categoryId) {
+    public Product(Long id, String name, String explanation, String price, String tax, Boolean isUsed,
+                   Boolean isSold, String categoryId) {
+        super(id);
         this.name = name;
         this.explanation = explanation;
         this.price = price;
         this.tax = tax;
-        this.registerDate = registerDate;
-        this.updateDate = updateDate;
         this.isUsed = isUsed;
         this.isSold = isSold;
         this.categoryId = categoryId;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
