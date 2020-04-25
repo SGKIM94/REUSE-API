d13d16bb40509f656d9292bc24f4135f24875554
sanggu <tkdrn8578@naver.com>
2020-04-25 16:34:05 +0900

    [ADD] Domain 의 Entity 의 id 및 createAt, updateAt 에 대한 common persist 를 관리하기 위한 AbstractEntity 추가

---
+++ src/main/java/reuse/domain/AbstractEntity.java
@@ -0,0 +1,80 @@
+package reuse.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    public AbstractEntity() {
+    }
+
+    public AbstractEntity(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFormattedCreateDate() {
+        return getFormattedDate(createAt, "yyyy.MM.dd HH:mm:ss");
+    }
+
+    public String getFormattedModifyDate() {
+        return getFormattedDate(updateAt, "yyyy.MM.dd HH:mm:ss");
+    }
+
+    private String getFormattedDate(LocalDateTime dateTime, String format) {
+        if (dateTime == null) {
+            return "";
+        }
+
+        return dateTime.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractEntity other = (AbstractEntity) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractEntity{" +
+                "id=" + id +
+                ", createdAt=" + createAt +
+                ", updatedAt=" + updateAt +
+                '}';
+    }
+}
