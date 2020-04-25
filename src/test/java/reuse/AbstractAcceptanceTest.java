752c54d4a4fe34e5e4c72b1ae2c147280058e7c8
sanggu <tkdrn8578@naver.com>
2020-04-25 16:40:45 +0900

    [MODIFY] 사용하지 않는 cleanAllDatabase 및 JdbcTemplate 제거

--- src/test/java/reuse/AbstractAcceptanceTest.java
+++ src/test/java/reuse/AbstractAcceptanceTest.java
@@ -3,8 +3,6 @@ package reuse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -12,10 +10,4 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 public class AbstractAcceptanceTest extends SoftAssertionTest {
     @Autowired
     public WebTestClient webTestClient;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    protected void cleanAllDatabases() {
-    }
 }
