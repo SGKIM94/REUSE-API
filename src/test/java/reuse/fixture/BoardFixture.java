ec02bb5e6b891e0169a669d6a0063d77a236f653
sanggu <tkdrn8578@naver.com>
2020-04-25 16:38:52 +0900

    [MODIFY] CommonFixture 추가에 따른 fixture 로직 수정

--- src/test/java/reuse/fixture/BoardFixture.java
+++ src/test/java/reuse/fixture/BoardFixture.java
@@ -5,17 +5,16 @@ import reuse.dto.board.CreateBoardRequestView;
 
 import static reuse.fixture.ProductFixture.TEST_PRODUCT;
 
-public class BoardFixture {
+public class BoardFixture extends CommonFixture {
     public static final String TEST_BOARD_CONTENT = "테스트 판매";
     public static final String TEST_SELLER_ADDRESS = "경기도 성남시";
     public static final String TEST_BOARD_TITLE = "테스트 판매 게시판";
 
     public static final Board TEST_BOARD
-            = Board.builder().title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
+            = Board.builder().id(DEFAULT_ID).title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
             .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
 
 
     public static final CreateBoardRequestView CREATE_BOARD_REQUEST_VIEW
-            = CreateBoardRequestView.builder().id(1L).title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
-            .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
+            = CreateBoardRequestView.builder().board(TEST_BOARD).build();
 }
--- src/test/java/reuse/fixture/ProductFixture.java
+++ src/test/java/reuse/fixture/ProductFixture.java
@@ -1,15 +1,18 @@
 package reuse.fixture;
 
 import reuse.domain.Product;
+import reuse.dto.product.CreateProductRequestView;
 
-public class ProductFixture {
+public class ProductFixture extends CommonFixture {
     public static final String TEST_PRODUCT_NAME = "핸드폰";
     public static final String TEST_PRODUCT_EXPLANATION = "테스트 상품";
     public static final String TEST_PRICE = "20000";
     public static final String TEST_TAX = "200";
 
     public static final Product TEST_PRODUCT = Product.builder()
-            .name(TEST_PRODUCT_NAME).explanation(TEST_PRODUCT_EXPLANATION)
+            .id(DEFAULT_ID).name(TEST_PRODUCT_NAME).explanation(TEST_PRODUCT_EXPLANATION)
             .price(TEST_PRICE).tax(TEST_TAX).isSold(false).isUsed(true).build();
 
+    public static final CreateProductRequestView CREATE_PRODUCT_REQUEST_DTO = CreateProductRequestView.builder()
+            .product(TEST_PRODUCT).build();
 }
