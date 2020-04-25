1de3a8d9155a8cfd32daeec3c689b6fee0016951
sanggu <tkdrn8578@naver.com>
2020-04-25 18:06:55 +0900

    [ADD] 품목을 생성하기 위한 service code 추가

---
+++ src/main/java/reuse/service/ProductService.java
@@ -0,0 +1,27 @@
+package reuse.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reuse.domain.Product;
+import reuse.dto.product.CreateProductRequestView;
+import reuse.dto.product.CreateProductResponseView;
+import reuse.repository.ProductRepository;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public CreateProductResponseView create(CreateProductRequestView product) {
+        Product savedProduct = productRepository.save(product.toEntity(product));
+        if (savedProduct == null) {
+            throw new IllegalArgumentException("Product create fail!");
+        }
+
+        return CreateProductResponseView.toDto(savedProduct);
+    }
+}
