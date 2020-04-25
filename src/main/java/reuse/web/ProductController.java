60173c56e4332fabef1f16df5b4f2eb13694fb3e
sanggu <tkdrn8578@naver.com>
2020-04-25 18:06:45 +0900

    [MODIFY] product create 의 mock api 를 제거하고 product api 로 변경

--- src/main/java/reuse/web/ProductController.java
+++ src/main/java/reuse/web/ProductController.java
@@ -2,25 +2,31 @@ package reuse.web;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reuse.domain.Product;
 import reuse.dto.product.CreateProductRequestView;
+import reuse.dto.product.CreateProductResponseView;
+import reuse.service.ProductService;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping
-    public ResponseEntity create(CreateProductRequestView product) {
-        return ResponseEntity.created(URI.create("/products/1")).build();
+    public ResponseEntity create(@RequestBody CreateProductRequestView product) {
+        CreateProductResponseView savedProduct = productService.create(product);
+        return ResponseEntity.created(URI.create("/products/" + savedProduct.getId())).build();
     }
 
     @GetMapping
-    public ResponseEntity list(Product product) {
+    public ResponseEntity list(@RequestBody Product product) {
         return ResponseEntity.ok().build();
     }
 }
