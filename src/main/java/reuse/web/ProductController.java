75582ad65173cef59679c15dd191098b8b0f6859
sanggu <tkdrn8578@naver.com>
2020-04-25 16:41:08 +0900

    [MODIFY] Product Acceptance test 를 위한 mock api 추가

--- src/main/java/reuse/web/ProductController.java
+++ src/main/java/reuse/web/ProductController.java
@@ -2,15 +2,24 @@ package reuse.web;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reuse.domain.Product;
+import reuse.dto.product.CreateProductRequestView;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     @PostMapping
+    public ResponseEntity create(CreateProductRequestView product) {
+        return ResponseEntity.created(URI.create("/products/1")).build();
+    }
+
+    @GetMapping
     public ResponseEntity list(Product product) {
         return ResponseEntity.ok().build();
     }
