cae92b97f6ca743f0bd0601d6a74899c63e821af
sanggu <tkdrn8578@naver.com>
2020-04-26 21:13:06 +0900

    [ADD] product 를 조회하기 위한 find, list 의 acceptance test code 추가

--- src/test/java/reuse/web/ProductAcceptanceTest.java
+++ src/test/java/reuse/web/ProductAcceptanceTest.java
@@ -1,16 +1,21 @@
 package reuse.web;
 
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import reuse.AbstractAcceptanceTest;
 import reuse.dto.product.CreateProductResponseView;
+import reuse.dto.product.FindProductResponseView;
+import reuse.dto.product.ListProductResponseView;
 import reuse.security.TokenAuthenticationService;
 
-import static reuse.fixture.ProductFixture.CREATE_PRODUCT_REQUEST_DTO;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static reuse.fixture.ProductFixture.*;
 
 public class ProductAcceptanceTest extends AbstractAcceptanceTest {
     public static final String PRODUCT_BASE_URL = "/products";
@@ -26,7 +31,7 @@ public class ProductAcceptanceTest extends AbstractAcceptanceTest {
 
     @DisplayName("품목을 추가가 가능한지")
     @Test
-    public void createProduct(SoftAssertions softly) {
+    public void createProduct() {
         //when
         EntityExchangeResult<CreateProductResponseView> expectResponse = restWebClientTest.postMethodAcceptance
                 (PRODUCT_BASE_URL, CREATE_PRODUCT_REQUEST_DTO, CreateProductResponseView.class);
@@ -35,6 +40,44 @@ public class ProductAcceptanceTest extends AbstractAcceptanceTest {
         HttpHeaders responseHeaders = expectResponse.getResponseHeaders();
 
         //then
-        softly.assertThat(responseHeaders.getLocation()).isNotNull();
+        assertThat(responseHeaders.getLocation()).isNotNull();
+    }
+
+    @DisplayName("품목 리스트를 조회 가능한지")
+    @Test
+    public void listProduct() {
+        //given
+        restWebClientTest.createProduct(CREATE_PRODUCT_REQUEST_DTO);
+        restWebClientTest.createProduct(SECOND_CREATE_PRODUCT_REQUEST_DTO);
+        //when
+        EntityExchangeResult<ListProductResponseView> response
+                = restWebClientTest.getMethodAcceptance(PRODUCT_BASE_URL, ListProductResponseView.class);
+
+        //then
+        HttpStatus status = response.getStatus();
+        ListProductResponseView responseBody = response.getResponseBody();
+        List<FindProductResponseView> products = responseBody.getProducts();
+
+        assertThat(status).isEqualByComparingTo(HttpStatus.OK);
+        assertThat(responseBody.getSize()).isEqualTo(2);
+        assertThat(products.get(0).getName()).isEqualTo(TEST_PRODUCT_NAME);
+        assertThat(products.get(1).getName()).isEqualTo(TEST_PRODUCT_NAME);
+    }
+
+    @DisplayName("품목 상세를 조회 가능한지")
+    @Test
+    public void findProduct() {
+        //given
+        restWebClientTest.createProduct(CREATE_PRODUCT_REQUEST_DTO);
+        //when
+        EntityExchangeResult<FindProductResponseView> response = restWebClientTest.getMethodAcceptance
+                (PRODUCT_BASE_URL + "/" + DEFAULT_ID, FindProductResponseView.class);
+
+        //then
+        HttpStatus status = response.getStatus();
+        FindProductResponseView responseBody = response.getResponseBody();
+
+        assertThat(status).isEqualByComparingTo(HttpStatus.OK);
+        assertThat(responseBody.getName()).isEqualTo(TEST_PRODUCT_NAME);
     }
 }
