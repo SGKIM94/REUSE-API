f368fb1e8841fbf75136e8a06660d196b44f02e8
sanggu <tkdrn8578@naver.com>
2020-04-27 23:22:23 +0900

    [ADD] list 에 대한 product fixture 추가

--- src/test/java/reuse/fixture/CommonFixture.java
+++ src/test/java/reuse/fixture/CommonFixture.java
@@ -2,4 +2,5 @@ package reuse.fixture;
 
 public class CommonFixture {
     public static final long DEFAULT_ID = 1L;
+    public static final long SECOND_ID = 2L;
 }
--- src/test/java/reuse/fixture/ProductFixture.java
+++ src/test/java/reuse/fixture/ProductFixture.java
@@ -3,6 +3,9 @@ package reuse.fixture;
 import reuse.domain.Product;
 import reuse.dto.product.CreateProductRequestView;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ProductFixture extends CommonFixture {
     public static final String TEST_PRODUCT_NAME = "핸드폰";
     public static final String TEST_PRODUCT_EXPLANATION = "테스트 상품";
@@ -13,6 +16,16 @@ public class ProductFixture extends CommonFixture {
             .id(DEFAULT_ID).name(TEST_PRODUCT_NAME).explanation(TEST_PRODUCT_EXPLANATION)
             .price(TEST_PRICE).tax(TEST_TAX).isSold(false).isUsed(true).build();
 
+    public static final Product SECOND_TEST_PRODUCT = Product.builder()
+            .id(SECOND_ID).name(TEST_PRODUCT_NAME).explanation(TEST_PRODUCT_EXPLANATION)
+            .price(TEST_PRICE).tax(TEST_TAX).isSold(false).isUsed(true).build();
+
     public static final CreateProductRequestView CREATE_PRODUCT_REQUEST_DTO = CreateProductRequestView.builder()
             .product(TEST_PRODUCT).build();
+
+    public static final CreateProductRequestView SECOND_CREATE_PRODUCT_REQUEST_DTO = CreateProductRequestView.builder()
+            .product(SECOND_TEST_PRODUCT).build();
+
+    public static final List<Product> LIST_PRODUCT_RESPONSE_VIEW
+            = Arrays.asList(TEST_PRODUCT, SECOND_TEST_PRODUCT);
 }
