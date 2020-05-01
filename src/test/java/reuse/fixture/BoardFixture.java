9b2e6308d7c5eb6a00cc95efec9f3b3d8fdc3b5f
sanggu <tkdrn8578@naver.com>
2020-05-01 23:19:27 +0900

    [ADD] Board 의 find, list test code 작성하기 위한 fixture 추가

--- src/test/java/reuse/fixture/BoardFixture.java
+++ src/test/java/reuse/fixture/BoardFixture.java
@@ -2,8 +2,14 @@ package reuse.fixture;
 
 import reuse.domain.Board;
 import reuse.dto.board.CreateBoardRequestView;
+import reuse.dto.board.FindBoardResponseView;
+import reuse.dto.board.ListBoardResponseView;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static reuse.fixture.ProductFixture.TEST_PRODUCT;
+import static reuse.fixture.UserFixture.TEST_USER;
 
 public class BoardFixture extends CommonFixture {
     public static final String TEST_BOARD_CONTENT = "테스트 판매";
@@ -14,7 +20,17 @@ public class BoardFixture extends CommonFixture {
             = Board.builder().id(DEFAULT_ID).title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
             .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
 
-
     public static final CreateBoardRequestView CREATE_BOARD_REQUEST_VIEW
             = CreateBoardRequestView.builder().board(TEST_BOARD).build();
+
+    public static final FindBoardResponseView FIND_BOARD_RESPONSE_VIEW
+            = FindBoardResponseView.builder().id(DEFAULT_ID).title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
+            .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).seller(TEST_USER).build();
+
+    public static final ListBoardResponseView LIST_BOARD_RESPONSE_VIEW
+             = ListBoardResponseView.builder().boards(Arrays.asList(FIND_BOARD_RESPONSE_VIEW, FIND_BOARD_RESPONSE_VIEW))
+            .build();
+
+    public static final List<Board> TEST_BOARDS
+            = Arrays.asList(TEST_BOARD, TEST_BOARD);
 }
