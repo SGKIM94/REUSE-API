6ed3311dc90fde1491769be14c0fb405984a70d2
sanggu <tkdrn8578@naver.com>
2020-05-03 22:47:59 +0900

    [ADD] Dto 로 만들어주기 위한 toDto 추가

--- src/main/java/reuse/dto/board/FindBoardResponseView.java
+++ src/main/java/reuse/dto/board/FindBoardResponseView.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import reuse.domain.Board;
 import reuse.domain.Product;
 import reuse.domain.User;
 
@@ -26,5 +27,16 @@ public class FindBoardResponseView {
         this.product = product;
         this.seller = seller;
         this.sellerAddress = sellerAddress;
+    }
+
+    public static FindBoardResponseView toDto(Board board) {
+        return FindBoardResponseView.builder()
+                .id(board.getId())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .product(board.getProduct())
+                .seller(board.getSeller())
+                .sellerAddress(board.getSellerAddress())
+                .build();
     }
 }
