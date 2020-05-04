6ca430dfcbc7cbfa76d81295b3852b7d8a6d2267
sanggu <tkdrn8578@naver.com>
2020-05-04 17:12:26 +0900

    [MODIFY] 사용자를 등록하는 endpoinst 추가

--- src/main/java/reuse/web/UserController.java
+++ src/main/java/reuse/web/UserController.java
@@ -3,11 +3,10 @@ package reuse.web;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reuse.domain.User;
 import reuse.dto.user.CreateUserRequestView;
 import reuse.dto.user.CreateUserResponseView;
+import reuse.dto.user.FindByIdResponseView;
 import reuse.dto.user.LoginUserRequestView;
-import reuse.security.LoginUser;
 import reuse.service.UserService;
 
 import java.net.URI;
@@ -21,13 +20,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("")
+    public ResponseEntity signUp(@RequestBody CreateUserRequestView user) {
+        CreateUserResponseView savedUser = userService.singUp(user);
+        return ResponseEntity.created(URI.create("/users/" + savedUser.getId())).build();
+    }
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginUserRequestView loginUser) {
         return ResponseEntity.ok().body(userService.login(loginUser));
     }
 
-    @GetMapping("")
-    public ResponseEntity detail(@LoginUser User user) {
-        return ResponseEntity.ok().body(userService.findById(user.getId()));
+    @GetMapping("/{id}")
+    public ResponseEntity detail(@PathVariable long id) {
+        FindByIdResponseView byId = userService.findById(id);
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
