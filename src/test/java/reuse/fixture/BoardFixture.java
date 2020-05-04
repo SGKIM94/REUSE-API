83bd72ab5ce5749182c0784a8ade327c728322e2
sanggu <tkdrn8578@naver.com>
2020-05-04 17:10:52 +0900

    [MODIFY] FindByIdResponseDto 의 이름 변경에 따른 fixture 수정

--- src/test/java/reuse/fixture/BoardFixture.java
+++ src/test/java/reuse/fixture/BoardFixture.java
@@ -4,6 +4,7 @@ import reuse.domain.Board;
 import reuse.dto.board.CreateBoardRequestView;
 import reuse.dto.board.FindBoardResponseView;
 import reuse.dto.board.ListBoardResponseView;
+import reuse.dto.board.ModifyBoardRequestView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,9 +16,14 @@ public class BoardFixture extends CommonFixture {
     public static final String TEST_BOARD_CONTENT = "테스트 판매";
     public static final String TEST_SELLER_ADDRESS = "경기도 성남시";
     public static final String TEST_BOARD_TITLE = "테스트 판매 게시판";
+    public static final String TEST_MODIFY_BOARD_TITLE = "수정된 판매 게시판";
 
     public static final Board TEST_BOARD
             = Board.builder().id(DEFAULT_ID).title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
+            .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
+
+    public static final Board MODIFY_TEST_BOARD
+            = Board.builder().id(DEFAULT_ID).title(TEST_MODIFY_BOARD_TITLE).content(TEST_BOARD_CONTENT)
             .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
 
     public static final CreateBoardRequestView CREATE_BOARD_REQUEST_VIEW
@@ -33,4 +39,7 @@ public class BoardFixture extends CommonFixture {
 
     public static final List<Board> TEST_BOARDS
             = Arrays.asList(TEST_BOARD, TEST_BOARD);
+
+    public static final ModifyBoardRequestView MODIFY_BOARD_REQUEST_DTO
+            = ModifyBoardRequestView.builder().board(MODIFY_TEST_BOARD).build();
 }
--- src/test/java/reuse/fixture/UserFixture.java
+++ src/test/java/reuse/fixture/UserFixture.java
@@ -2,9 +2,11 @@ package reuse.fixture;
 
 import reuse.domain.User;
 import reuse.dto.user.CreateUserRequestView;
-import reuse.dto.user.FindByEmailResponseView;
+import reuse.dto.user.FindByIdResponseView;
 import reuse.dto.user.FindBySocialTokenIdResponseView;
 import reuse.dto.user.LoginUserRequestView;
+
+import static reuse.fixture.CommonFixture.DEFAULT_ID;
 
 public class UserFixture {
     public static final Long KIM_ID = 1L;
@@ -14,9 +16,9 @@ public class UserFixture {
     public static final String SOCIAL_TOKEN_ID = "tokenId";
     public static final String NAVER_SOCIAL_TYPE = "naver";
 
-    public static final CreateUserRequestView USER_SIGH_UP_REQUEST_DTO = new CreateUserRequestView(1L, KIM_EMAIL, KIM_NAME, KIM_PASSWORD);
+    public static final CreateUserRequestView USER_SIGH_UP_REQUEST_DTO = new CreateUserRequestView(1L, SOCIAL_TOKEN_ID, KIM_EMAIL, KIM_NAME);
     public static final LoginUserRequestView USER_LOGIN_REQUEST_DTO = new LoginUserRequestView(KIM_EMAIL, KIM_PASSWORD);
-    public static final FindByEmailResponseView FIND_BY_EMAIL_RESPONSE_VIEW = new FindByEmailResponseView(KIM_ID, KIM_EMAIL, KIM_PASSWORD);
+    public static final FindByIdResponseView FIND_BY_EMAIL_RESPONSE_VIEW = new FindByIdResponseView(KIM_ID, KIM_EMAIL, KIM_PASSWORD);
     public static final FindBySocialTokenIdResponseView FIND_BY_SOCIAL_TOKEN_ID_RESPONSE_VIEW = new FindBySocialTokenIdResponseView(KIM_ID, SOCIAL_TOKEN_ID, NAVER_SOCIAL_TYPE);
 
     public static CreateUserRequestView getCreateUserRequestView(User user) {
@@ -28,5 +30,5 @@ public class UserFixture {
     }
 
     public static final User TEST_USER = new User(KIM_ID, SOCIAL_TOKEN_ID, KIM_NAME, NAVER_SOCIAL_TYPE);
-    public static final User LOGIN_USER = User.builder().socialTokenId(SOCIAL_TOKEN_ID).socialType(NAVER_SOCIAL_TYPE).build();
+    public static final User LOGIN_USER = User.builder().id(DEFAULT_ID).socialTokenId(SOCIAL_TOKEN_ID).socialType(NAVER_SOCIAL_TYPE).build();
 }
