3ab6570d3b63f0f57f1780055387bb10dfbe46e2
kimsanggu <tkdrn8578@paymint.co.kr>
2020-05-08 18:31:54 +0900

    [MODIFY] 사용자 로그인 시 name 도 저장 후 리턴하도록 DTO 에 name 추가

--- src/main/java/reuse/dto/user/LoginUserResponseView.java
+++ src/main/java/reuse/dto/user/LoginUserResponseView.java
@@ -13,12 +13,15 @@ public class LoginUserResponseView {
     private String socialTokenId;
     private String socialType;
     private String jwt;
+    private String name;
+
 
     @Builder
-    public LoginUserResponseView(String socialTokenId, String socialType, String jwt) {
+    public LoginUserResponseView(String socialTokenId, String socialType, String jwt, String name) {
         this.socialTokenId = socialTokenId;
         this.socialType = socialType;
         this.jwt = jwt;
+        this.name = name;
     }
 
     public static LoginUserResponseView toDto(String jwt, User user) {
@@ -26,6 +29,7 @@ public class LoginUserResponseView {
                 .socialTokenId(user.getSocialTokenId())
                 .socialType(user.getSocialType())
                 .jwt(jwt)
+                .name(user.getName())
                 .build();
     }
 }
