714446d0ce11082cde2cf9a57e42e44074404d70
sanggu <tkdrn8578@naver.com>
2020-05-09 15:19:49 +0900

    [ADD] file 을 create 하는 init 메서드 및 mock store service code 추가

--- src/main/java/reuse/storage/FileSystemStorageService.java
+++ src/main/java/reuse/storage/FileSystemStorageService.java
@@ -1,8 +1,14 @@
 package reuse.storage;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Service
 public class FileSystemStorageService {
     private final Path rootLocation;
 
@@ -10,7 +16,19 @@ public class FileSystemStorageService {
         this.rootLocation = Paths.get(properties.getLocation());
     }
 
-    public Path load(String s) {
-        return rootLocation.resolve(s);
+    public Path load(String fileName) {
+        return rootLocation.resolve(fileName);
+    }
+
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
+    }
+
+    public void store(MultipartFile file) {
+
     }
 }
