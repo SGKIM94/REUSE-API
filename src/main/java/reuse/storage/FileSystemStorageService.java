7b310487ff4d9b1e159a8947f22f5b3e08e59ccd
sanggu <tkdrn8578@naver.com>
2020-05-09 15:32:22 +0900

    [ADD] file 을 save 하고 load 하는 service production code 추가

--- src/main/java/reuse/storage/FileSystemStorageService.java
+++ src/main/java/reuse/storage/FileSystemStorageService.java
@@ -1,12 +1,15 @@
 package reuse.storage;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileSystemStorageService {
@@ -29,6 +32,23 @@ public class FileSystemStorageService {
     }
 
     public void store(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + fileName);
+            }
+
+            if (fileName.contains("..")) {
+                throw new StorageException("Cannot store file with relative path outside current directory" + fileName);
+            }
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, this.rootLocation.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+            }
+
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file : " + fileName, e);
+        }
     }
 }
