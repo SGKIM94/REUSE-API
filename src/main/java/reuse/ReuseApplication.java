8f7ac9aec5f949dfcc45242d1c5b47f693f95737
sanggu <tkdrn8578@naver.com>
2020-05-09 15:18:44 +0900

    [ADD] 파일 저장 시 root location 정보 property 설정을 추가하기 위한 @EnableConfigurationProperties 추가

--- src/main/java/reuse/ReuseApplication.java
+++ src/main/java/reuse/ReuseApplication.java
@@ -2,10 +2,13 @@ package reuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import reuse.storage.StorageProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(StorageProperties.class)
 public class ReuseApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReuseApplication.class, args);
