0c963a5b84da3224adf14d73fcb8b849d04c07a5
sanggu <tkdrn8578@naver.com>
2020-05-10 22:53:26 +0900

    [ADD] 품목 이미지를 저장하는 테스트를 하기 위한 fixture 추가

--- src/test/java/reuse/fixture/ProductFixture.java
+++ src/test/java/reuse/fixture/ProductFixture.java
@@ -1,5 +1,8 @@
 package reuse.fixture;
 
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 import reuse.domain.Product;
 import reuse.dto.product.CreateProductRequestView;
 
@@ -11,6 +14,8 @@ public class ProductFixture extends CommonFixture {
     public static final String TEST_PRODUCT_EXPLANATION = "테스트 상품";
     public static final String TEST_PRICE = "20000";
     public static final String TEST_TAX = "200";
+    public static final String TEST_IMAGE_FILE_NAME1 = "foo1.txt";
+    public static final String TEST_IMAGE_FILE_NAME2 = "foo2.txt";
 
     public static final Product TEST_PRODUCT = Product.builder()
             .id(DEFAULT_ID).name(TEST_PRODUCT_NAME).explanation(TEST_PRODUCT_EXPLANATION)
@@ -20,8 +25,15 @@ public class ProductFixture extends CommonFixture {
             .id(SECOND_ID).name(TEST_PRODUCT_NAME).explanation(TEST_PRODUCT_EXPLANATION)
             .price(TEST_PRICE).tax(TEST_TAX).isSold(false).isUsed(true).build();
 
+    public static final MultipartFile TEST_IMAGE1
+            = new MockMultipartFile("foo1", TEST_IMAGE_FILE_NAME1, MediaType.TEXT_PLAIN_VALUE, "test file".getBytes());
+    public static final MultipartFile TEST_IMAGE2
+            = new MockMultipartFile("foo2", TEST_IMAGE_FILE_NAME2, MediaType.TEXT_PLAIN_VALUE, "test file".getBytes());
+
+    public static final List<MultipartFile> TEST_IMAGES = Arrays.asList(TEST_IMAGE1, TEST_IMAGE2);
+
     public static final CreateProductRequestView CREATE_PRODUCT_REQUEST_DTO = CreateProductRequestView.builder()
-            .product(TEST_PRODUCT).build();
+            .product(TEST_PRODUCT).productImages(TEST_IMAGES).build();
 
     public static final CreateProductRequestView SECOND_CREATE_PRODUCT_REQUEST_DTO = CreateProductRequestView.builder()
             .product(SECOND_TEST_PRODUCT).build();
