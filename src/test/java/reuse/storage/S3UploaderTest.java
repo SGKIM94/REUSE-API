18a03eddf4889c10500aa88eeb1d582e7d4f0765
kimsanggu <tkdrn8578@paymint.co.kr>
2020-05-15 15:08:07 +0900

    [ADD] S3에 파일을 upload 하는 method 에 대한 test code 추가

--- src/test/java/reuse/storage/S3UploaderTest.java
+++ src/test/java/reuse/storage/S3UploaderTest.java
@@ -1,26 +1,44 @@
 package reuse.storage;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static reuse.fixture.ProductFixture.TEST_IMAGE1;
+import static reuse.fixture.ProductFixture.TEST_IMAGE_FILE_NAME1;
 
 public class S3UploaderTest {
+    private S3Uploader s3Uploader;
+
+    @BeforeEach
+    public void setup() {
+        this.s3Uploader = new S3Uploader();
+    }
 
     @DisplayName("MultipartFile 을 File 타입으로 변경하는지")
     @Test
-    public void  convert() {
-        //given
-        Optional<File> convertedFIle = S3Uploader.convert(TEST_IMAGE1);
-        File file = convertedFIle.orElseThrow();
-
+    public void  convert() throws IOException {
         //when
+        Optional<File> convertedFIle = s3Uploader.convert(TEST_IMAGE1);
+        File file = convertedFIle.orElseThrow();
 
         //then
         assertThat(file).isFile();
     }
+
+    @DisplayName("MultipartFile 을 S3에 업로드하는지")
+    @Test
+    public void  upload() throws IOException {
+        //when
+        String uploadFileName = s3Uploader.upload(TEST_IMAGE1, TEST_IMAGE_FILE_NAME1);
+
+        //then
+        assertThat(uploadFileName).isNotBlank();
+    }
+
 }
