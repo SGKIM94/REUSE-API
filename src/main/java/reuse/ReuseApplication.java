135b630503a91ab11de4b0267fb89f264564f486
kimsanggu <tkdrn8578@paymint.co.kr>
2020-05-15 17:32:05 +0900

    [MODIFY] yml 이 2개로 추가됨에 따른 임의로 application 이 upload 할 때 부를 수 있도록 수정

--- src/main/java/reuse/ReuseApplication.java
+++ src/main/java/reuse/ReuseApplication.java
@@ -1,7 +1,7 @@
 package reuse;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import reuse.storage.StorageProperties;
@@ -10,10 +10,16 @@ import reuse.storage.StorageProperties;
 @EnableJpaAuditing
 @EnableConfigurationProperties(StorageProperties.class)
 public class ReuseApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ReuseApplication.class, args);
-    }
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ReuseApplication.class)
+                .profiles(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 //    @Bean
 //    CommandLineRunner init(FileSystemStorageService storageService) {
 //        return (args) -> {
