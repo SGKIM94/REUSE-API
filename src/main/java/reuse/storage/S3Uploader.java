2ca5e32521f42d6675a36a5caa6265693aa209ba
sanggu <tkdrn8578@naver.com>
2020-05-19 23:29:51 +0900

    [MODIFY] laguage 10 설정으로 인해 발생하는 오류로 수정

--- src/main/java/reuse/storage/S3Uploader.java
+++ src/main/java/reuse/storage/S3Uploader.java
@@ -1,6 +1,6 @@
 package reuse.storage;
 
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
@@ -16,7 +16,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -24,7 +23,7 @@ import java.util.stream.Collectors;
 @Component
 public class S3Uploader {
 
-    private final AmazonS3Client amazonS3Client;
+    private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
@@ -34,14 +33,14 @@ public class S3Uploader {
     }
 
     public List<String> getFiles(Long productId) {
-        ObjectListing files = amazonS3Client.listObjects(bucket, "products/" + productId + "/");
+        ObjectListing files = amazonS3.listObjects(bucket, "products/" + productId + "/");
         return files.getObjectSummaries().stream()
             .map(S3ObjectSummary::getKey)
             .collect(Collectors.toList());
     }
 
 
-    public Optional<File> convert(MultipartFile image) throws IOException {
+    public File convert(MultipartFile image) throws IOException {
         if (image == null || image.isEmpty() || StringUtils.isEmpty(image.getOriginalFilename())) {
             throw new StorageException("file is empty when convert file!");
         }
@@ -52,15 +51,15 @@ public class S3Uploader {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                 fos.write(image.getBytes());
             }
-            return Optional.of(convertFile);
+            return convertFile;
         }
 
-        return Optional.empty();
+        throw new StorageException("Fail convert file");
     }
 
     public String upload(MultipartFile image, String directoryName) {
         try {
-            File uploadFile = convert(image).orElseThrow(() -> new StorageException("file is invalid"));
+            File uploadFile = convert(image);
             return upload(uploadFile, directoryName);
         } catch (IOException e) {
             throw new StorageException("file file upload!");
@@ -75,10 +74,10 @@ public class S3Uploader {
     }
 
     public String putImageToS3(File image, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, image)
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, image)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
 
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+        return amazonS3.getUrl(bucket, fileName).toString();
     }
 
     public void removeNewFile(File targetFile) {
