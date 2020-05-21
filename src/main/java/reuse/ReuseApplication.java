757e4eb34ea76d7432a351fa9b0f07cbb6ca5fa1
kimsanggu <tkdrn8578@paymint.co.kr>
2020-05-21 18:25:31 +0900

    [MODIFY] application.yml 에 설정 정보 옮김에 따른 수정

--- src/main/java/reuse/ReuseApplication.java
+++ src/main/java/reuse/ReuseApplication.java
@@ -1,7 +1,7 @@
 package reuse;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import reuse.storage.StorageProperties;
@@ -10,13 +10,7 @@ import reuse.storage.StorageProperties;
 @EnableJpaAuditing
 @EnableConfigurationProperties(StorageProperties.class)
 public class ReuseApplication {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:aws.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ReuseApplication.class)
-                .profiles(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(ReuseApplication.class, args);
     }
 }
