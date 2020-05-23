7f64c121913086a43a0520e0b6e4b8b70e20d529
sanggu <tkdrn8578@naver.com>
2020-05-23 15:52:45 +0900

    [ADD] MULTIPART-FORM-DATA 로 request 를 요청하기 위해 MultiValueMap<String, Object> type 의 fixture 추가

--- src/test/java/reuse/fixture/ProductFixture.java
+++ src/test/java/reuse/fixture/ProductFixture.java
@@ -2,6 +2,8 @@ package reuse.fixture;
 
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 import reuse.domain.Product;
 import reuse.dto.product.CreateProductRequestView;
@@ -25,7 +27,8 @@ public class ProductFixture extends CommonFixture {
             .id(SECOND_ID).name(TEST_PRODUCT_NAME).explanation(TEST_PRODUCT_EXPLANATION)
             .price(TEST_PRICE).tax(TEST_TAX).isSold(false).isUsed(true).build();
 
-    public static final MultipartFile TEST_IMAGE1
+    public static final MultipartFile
+            TEST_IMAGE1
             = new MockMultipartFile("foo1", TEST_IMAGE_FILE_NAME1, MediaType.TEXT_PLAIN_VALUE, "test file".getBytes());
     public static final MultipartFile TEST_IMAGE2
             = new MockMultipartFile("foo2", TEST_IMAGE_FILE_NAME2, MediaType.TEXT_PLAIN_VALUE, "test file".getBytes());
@@ -33,11 +36,37 @@ public class ProductFixture extends CommonFixture {
     public static final List<MultipartFile> TEST_IMAGES = Arrays.asList(TEST_IMAGE1, TEST_IMAGE2);
 
     public static final CreateProductRequestView CREATE_PRODUCT_REQUEST_DTO = CreateProductRequestView.builder()
-            .product(TEST_PRODUCT).productImages(TEST_IMAGES).productImage(TEST_IMAGE1).build();
+            .product(TEST_PRODUCT).productImage(TEST_IMAGE1).build();
 
     public static final CreateProductRequestView SECOND_CREATE_PRODUCT_REQUEST_DTO = CreateProductRequestView.builder()
             .product(SECOND_TEST_PRODUCT).build();
 
     public static final List<Product> LIST_PRODUCT_RESPONSE_VIEW
             = Arrays.asList(TEST_PRODUCT, SECOND_TEST_PRODUCT);
+
+    public static MultiValueMap<String, Object> getCreateProductMap() {
+        MultiValueMap<String, Object> product = new LinkedMultiValueMap<>();
+        product.add("id", DEFAULT_ID);
+        product.add("name", TEST_PRODUCT_NAME);
+        product.add("explanation", TEST_PRODUCT_EXPLANATION);
+        product.add("price", TEST_PRICE);
+        product.add("tax", TEST_TAX);
+        product.add("isUsed", true);
+        product.add("isSold", true);
+
+        return product;
+    }
+
+    public static MultiValueMap<String, Object> getSecondCreateProductMap() {
+        MultiValueMap<String, Object> product = new LinkedMultiValueMap<>();
+        product.add("id", SECOND_ID);
+        product.add("name", TEST_PRODUCT_NAME);
+        product.add("explanation", TEST_PRODUCT_EXPLANATION);
+        product.add("price", TEST_PRICE);
+        product.add("tax", TEST_TAX);
+        product.add("isUsed", true);
+        product.add("isSold", true);
+
+        return product;
+    }
 }
