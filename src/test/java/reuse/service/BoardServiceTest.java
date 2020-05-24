0a6df12331dc5e101ea25c5b85cd6a0dace786cb
sanggu <tkdrn8578@naver.com>
2020-05-24 22:21:43 +0900

    [ADD] Board 를 조회하는 findById 에 대한 정상 및 예외의 test code 추가

--- src/test/java/reuse/service/BoardServiceTest.java
+++ src/test/java/reuse/service/BoardServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import reuse.dto.board.CreateBoardResponseView;
+import reuse.dto.board.FindBoardResponseView;
 import reuse.dto.board.ListBoardResponseView;
 import reuse.repository.BoardRepository;
 import reuse.repository.ProductRepository;
@@ -14,7 +15,9 @@ import reuse.security.TokenAuthenticationService;
 import reuse.storage.S3Uploader;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static reuse.fixture.BoardFixture.*;
 import static reuse.fixture.ProductFixture.TEST_PRODUCT;
@@ -57,7 +60,6 @@ public class BoardServiceTest {
         assertThat(board.getSellerAddress()).isEqualTo(TEST_SELLER_ADDRESS);
     }
 
-    @Disabled
     @DisplayName("게시물 리스트르 조회하는지")
     @Test
     public void list() {
@@ -68,12 +70,37 @@ public class BoardServiceTest {
         assertThat(boards.getSize()).isEqualTo(2);
     }
 
+    @DisplayName("특정 게시물을 ID 로 조회 가능한지")
+    @Test
+    public void findById() {
+        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BOARD));
+
+        FindBoardResponseView board = boardService.retrieve(DEFAULT_ID);
+
+        assertThat(board.getContent()).isEqualTo(TEST_BOARD_CONTENT);
+        verify(boardRepository).findById(any());
+    }
+
+
+    @DisplayName("게시물 상세 조회 시 존재하지 않는 경우 예외를 던지는지")
+    @Test
+    public void findByIdFail() {
+        when(boardRepository.findById(any())).thenThrow(IllegalArgumentException.class);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            boardService.retrieve(DEFAULT_ID);
+        });
+    }
+
     @Disabled
     @DisplayName("게시물 수정이 가능한지")
     @Test
     public void update() {
+        // TODO: 검증방법 고려하기
         boardService.modify(MODIFY_BOARD_REQUEST_DTO);
 
-        // find 추가 후 검증하는 메서드필요
+//        Board modifiedBoard = boardService.findById(DEFAULT_ID);
+
+//        assertThat(modifiedBoard.getTitle()).isEqualTo(TEST_MODIFY_BOARD_TITLE);
     }
 }
