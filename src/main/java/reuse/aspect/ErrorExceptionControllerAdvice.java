754e90306c44e66930794ade40aad5e4935f5e41
kimsanggu <tkdrn8578@paymint.co.kr>
2020-05-29 18:36:19 +0900

    [MODIFY] DTO validation 시 발생한 예외 처리 추가

--- src/main/java/reuse/aspect/ErrorExceptionControllerAdvice.java
+++ src/main/java/reuse/aspect/ErrorExceptionControllerAdvice.java
@@ -3,6 +3,7 @@ package reuse.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import reuse.storage.StorageException;
@@ -24,5 +25,12 @@ public class ErrorExceptionControllerAdvice {
         log.error(String.valueOf(e.getCause()));
 
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
