3ebc40fa14eb1eb3686159aa34f647efed96e7b8
kimsanggu <tkdrn8578@paymint.co.kr>
2020-05-29 19:03:08 +0900

    [MODIFY] Board create 시 User 를 조회하는 로직 추가 중간 커밋

--- src/main/java/reuse/service/BoardService.java
+++ src/main/java/reuse/service/BoardService.java
@@ -4,21 +4,26 @@ import org.springframework.stereotype.Service;
 import reuse.domain.Board;
 import reuse.domain.Product;
 import reuse.dto.board.*;
+import reuse.dto.user.FindByIdResponseView;
 import reuse.repository.BoardRepository;
 
 @Service
 public class BoardService {
     private BoardRepository boardRepository;
     private ProductService productService;
+    private UserService userService;
 
-    public BoardService(BoardRepository boardRepository, ProductService productService) {
+    public BoardService(BoardRepository boardRepository, ProductService productService, UserService userService) {
         this.boardRepository = boardRepository;
         this.productService = productService;
+        this.userService = userService;
     }
 
     public CreateBoardResponseView create(CreateBoardRequestView board) {
         Product product = productService.findById(board.getProductId());
-        Board savedBoard = boardRepository.save(CreateBoardRequestView.toEntity(board, product));
+        //TODO : 게시판에 사용자 정보를 저장하기 위해 조회하거나 LoginUser annotation 사용하도록 필요
+        FindByIdResponseView user = userService.findById(board.getUserId());
+        Board savedBoard = boardRepository.save(CreateBoardRequestView.toEntity(board, product, user.));
         return CreateBoardResponseView.toDto(savedBoard);
     }
 
