4a510f0e8e035a4a7bb626d5e89c8c79f17ca9e1
sanggu <tkdrn8578@naver.com>
2020-05-30 14:15:45 +0900

    [ADD] clean test dummy 를 모듈 올라오기 전 실행

--- src/test/java/reuse/AbstractAcceptanceTest.java
+++ src/test/java/reuse/AbstractAcceptanceTest.java
@@ -3,10 +3,12 @@ package reuse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "100000")
+@Sql(scripts = {"/clean-test-all.sql"})
 public class AbstractAcceptanceTest extends SoftAssertionTest {
     @Autowired
     public WebTestClient webTestClient;
