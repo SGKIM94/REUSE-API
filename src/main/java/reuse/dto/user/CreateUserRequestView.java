b4fd6ed2e6361465cfe68da17df2ad588c0cb339
sanggu <tkdrn8578@naver.com>
2020-05-30 15:53:34 +0900

    [MODIFY] User 생성 시 id 및 email 을 받지 않고, socialType 을 받도록 변경

--- src/main/java/reuse/dto/user/CreateUserRequestView.java
+++ src/main/java/reuse/dto/user/CreateUserRequestView.java
@@ -1,5 +1,6 @@
 package reuse.dto.user;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,19 +10,21 @@ import reuse.domain.User;
 @Setter
 @NoArgsConstructor
 public class CreateUserRequestView {
-    private Long id;
     private String socialTokenId;
-    private String email;
+    private String socialType;
     private String name;
 
-    public CreateUserRequestView(Long id, String socialTokenId, String email, String name) {
-        this.id = id;
+    @Builder
+    public CreateUserRequestView(String socialTokenId, String name) {
         this.socialTokenId = socialTokenId;
-        this.email = email;
         this.name = name;
     }
 
     public static User toEntity(CreateUserRequestView newUser) {
-        return new User();
+        return User.builder()
+                .socialType(newUser.getSocialType())
+                .socialTokenId(newUser.getSocialTokenId())
+                .name(newUser.getName())
+                .build();
     }
 }
