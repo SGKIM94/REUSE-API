423af4b01b9db6d999d9dd7b9cffa87a1758a06c
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-01 18:02:02 +0900

    [ADD] 품목의 카테고리를 관리하기 위한 Category domain 추가

---
+++ src/main/java/reuse/domain/Category.java
@@ -0,0 +1,25 @@
+package reuse.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Category extends AbstractEntity {
+    private String teleco;
+
+    private String manufacturer;
+
+    private String model;
+
+    @Builder
+    public Category(String teleco, String manufacturer, String model) {
+        this.teleco = teleco;
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+}
