167e15f315def90bac5f8110cccd1005fd63bd71
sanggu <tkdrn8578@naver.com>
2020-06-01 21:44:18 +0900

    [ADD] category test 를 위한 fixture 추가

--- src/test/java/reuse/fixture/CategoryFixture.java
+++ src/test/java/reuse/fixture/CategoryFixture.java
@@ -1,8 +1,16 @@
 package reuse.fixture;
 
+import reuse.domain.Category;
 import reuse.dto.category.CreateCategoryRequestView;
 
 public class CategoryFixture extends CommonFixture {
+    public static final String TEST_TELECO = "1";
+    public static final String TEST_MANUFACTURER = "1";
+    public static final String TEST_MODEL = "APPLE";
+
     public static final CreateCategoryRequestView CREATE_CATEGORY_REQUEST_VIEW = CreateCategoryRequestView.builder()
             .teleco("0").manufacturer("0").model("0").build();
+
+    public static final Category TEST_CATEGORY
+            = Category.builder().teleco(TEST_TELECO).manufacturer(TEST_MANUFACTURER).model(TEST_MODEL).build();
 }
