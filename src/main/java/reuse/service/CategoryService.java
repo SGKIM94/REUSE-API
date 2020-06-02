9df01b709ec64b347468e6fa1de75c360a9b1d10
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-02 20:37:12 +0900

    [MODIFY] category retrieve 하기 위한 service code 추가

--- src/main/java/reuse/service/CategoryService.java
+++ src/main/java/reuse/service/CategoryService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import reuse.domain.Category;
 import reuse.dto.category.CreateCategoryRequestView;
 import reuse.dto.category.CreateCategoryResponseView;
+import reuse.dto.category.FindCategoryResponseView;
 import reuse.repository.CategoryRepository;
 
 @Service
@@ -14,9 +15,17 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
-
     public CreateCategoryResponseView create(CreateCategoryRequestView category) {
         Category savedCategory = categoryRepository.save(category.toEntity());
         return CreateCategoryResponseView.toDto(savedCategory);
+    }
+
+    public Category findById(long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 카테고리가 존재하지 않습니다."));
+    }
+
+    public FindCategoryResponseView retrieve(long id) {
+        return FindCategoryResponseView.toDto(findById(id));
     }
 }
