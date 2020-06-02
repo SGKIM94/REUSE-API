085868cb730e9f67590d0bcf225da6b9b5be2224
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-02 20:04:26 +0900

    [MODIFY] category 를 retrieve 하는 acceptance test code 추가

--- src/test/java/reuse/service/CategoryServiceTest.java
+++ src/test/java/reuse/service/CategoryServiceTest.java
@@ -6,14 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import reuse.dto.category.CreateCategoryResponseView;
+import reuse.dto.category.FindCategoryResponseView;
 import reuse.repository.CategoryRepository;
 import reuse.security.TokenAuthenticationService;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static reuse.fixture.CategoryFixture.CREATE_CATEGORY_REQUEST_VIEW;
-import static reuse.fixture.CategoryFixture.TEST_CATEGORY;
+import static reuse.fixture.CategoryFixture.*;
+import static reuse.fixture.CommonFixture.DEFAULT_ID;
 
 @SpringBootTest
 public class CategoryServiceTest {
@@ -39,5 +42,17 @@ public class CategoryServiceTest {
         CreateCategoryResponseView category = categoryService.create(CREATE_CATEGORY_REQUEST_VIEW);
 
         assertThat(category.getId()).isNotNull();
+    }
+
+    @DisplayName("게시물이 조회가 되는지")
+    @Test
+    public void retrieve() {
+        when(categoryRepository.findById(any())).thenReturn(Optional.of(TEST_CATEGORY));
+
+        FindCategoryResponseView category = categoryService.retrieve(DEFAULT_ID);
+
+        assertThat(category.getTelco()).isEqualTo(TEST_TELECO);
+        assertThat(category.getManufacturer()).isEqualTo(TEST_MANUFACTURER);
+        assertThat(category.getModel()).isEqualTo(TEST_MODEL);
     }
 }
