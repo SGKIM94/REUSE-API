7ce32f9c46a53731dc3b5b8dd6e4d135b349e28d
sanggu <tkdrn8578@naver.com>
2020-06-06 15:30:02 +0900

    [MODIFY] 게시물을 category  별로 조회하기 위한 querydsl 추가

--- src/main/java/reuse/repository/BoardRepositorySupport.java
+++ src/main/java/reuse/repository/BoardRepositorySupport.java
@@ -1,11 +1,17 @@
 package reuse.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 import reuse.domain.Board;
 import reuse.domain.Category;
-import reuse.dto.board.ListBoardResponseView;
+import reuse.domain.QCategory;
+import reuse.domain.QProduct;
+import reuse.dto.board.FindAllByCategoryResponseView;
+import reuse.dto.board.FindAllByCategoryResponseViews;
+
+import java.util.List;
 
 import static reuse.domain.QBoard.board;
 
@@ -18,10 +24,37 @@ public class BoardRepositorySupport extends QuerydslRepositorySupport {
         this.jpaQueryFactory = jpaQueryFactory;
     }
 
-    public ListBoardResponseView findAllByCategory(Category category) {
-        return jpaQueryFactory
-                .selectFrom(product)
-                .innerJoin()
-                .where(board.)
+    public FindAllByCategoryResponseViews findAllByCategory(Category requestCategory) {
+        QProduct product = QProduct.product;
+        QCategory category = QCategory.category;
+
+        List<FindAllByCategoryResponseView> findAll = jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                FindAllByCategoryResponseView.class,
+                                board.id,
+                                board.content,
+                                board.title,
+                                board.createAt,
+                                board.updateAt,
+                                board.seller,
+                                board.sellerAddress,
+                                product.thumbnailImage,
+                                product.isSold,
+                                product.isUsed,
+                                product.price,
+                                product.name,
+                                product.quality
+                        )
+                )
+                .innerJoin(board.product, product)
+                .innerJoin(product.category, category)
+                .where(
+                        category.teleco.eq(requestCategory.getTeleco())
+                                .or(category.manufacturer.eq(requestCategory.getManufacturer()))
+                                .or(category.model.eq(requestCategory.getModel()))
+                ).fetch();
+
+        return FindAllByCategoryResponseViews.toDto(findAll);
     }
 }
