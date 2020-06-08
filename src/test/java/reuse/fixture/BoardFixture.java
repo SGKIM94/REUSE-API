266f04d997fa834c9c092bdcf2cdb7000629e152
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-08 18:00:12 +0900

    [MODIFY] listByCategory 에 대한 fixture 추가

--- src/test/java/reuse/fixture/BoardFixture.java
+++ src/test/java/reuse/fixture/BoardFixture.java
@@ -1,14 +1,12 @@
 package reuse.fixture;
 
 import reuse.domain.Board;
-import reuse.dto.board.CreateBoardRequestView;
-import reuse.dto.board.FindBoardResponseView;
-import reuse.dto.board.ListBoardResponseView;
-import reuse.dto.board.ModifyBoardRequestView;
+import reuse.dto.board.*;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static reuse.fixture.CategoryFixture.*;
 import static reuse.fixture.ProductFixture.TEST_PRODUCT;
 import static reuse.fixture.ProductFixture.TEST_PRODUCT_ID;
 import static reuse.fixture.UserFixture.TEST_USER;
@@ -21,7 +19,14 @@ public class BoardFixture extends CommonFixture {
     public static final String TEST_SECOND_BOARD_TITLE = "테스트 판매 두번째 게시판";
     public static final String TEST_MODIFY_BOARD_TITLE = "수정된 판매 게시판";
 
+    public static final long FIRST_BOARD_ID = 1L;
+    public static final long SIXTH_BOARD_ID = 6L;
+
     public static final Board TEST_BOARD
+            = Board.builder().title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
+            .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
+
+    public static final Board TEST_SECOND_BOARD
             = Board.builder().title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
             .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).build();
 
@@ -50,4 +55,19 @@ public class BoardFixture extends CommonFixture {
 
     public static final ModifyBoardRequestView MODIFY_BOARD_REQUEST_DTO
             = ModifyBoardRequestView.builder().board(MODIFY_TEST_BOARD).build();
+
+    public static final FindByCategoryResponseView FIND_BY_CATEGORY_RESPONSE_VIEW
+            = FindByCategoryResponseView.builder().board(TEST_BOARD).build();
+
+    public static final FindByCategoryResponseView SECOND_FIND_BY_CATEGORY_RESPONSE_VIEW
+            = FindByCategoryResponseView.builder().board(TEST_BOARD).build();
+
+    public static final ListBoardByCategoryResponseView LIST_BOARD_BY_CATEGORY_RESPONSE_VIEW
+            = ListBoardByCategoryResponseView.builder()
+            .boards(Arrays.asList(FIND_BY_CATEGORY_RESPONSE_VIEW, SECOND_FIND_BY_CATEGORY_RESPONSE_VIEW))
+            .build();
+
+    public static final ListBoardByCategoryRequestView LIST_BOARD_BY_CATEGORY_REQUEST_VIEW
+             = ListBoardByCategoryRequestView.builder().manufacturer(TEST_MANUFACTURER).model(TEST_MODEL)
+            .teleco(TEST_TELECO).build();
 }
