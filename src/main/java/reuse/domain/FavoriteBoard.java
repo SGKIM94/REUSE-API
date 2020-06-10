c6fab865560d176d44852f7bd9887cc3f090a3a1
sanggu <tkdrn8578@naver.com>
2020-06-10 23:28:04 +0900

    [ADD] test 검증 시 사용하기 위한 getUser 및 getBoard 추가

--- src/main/java/reuse/domain/FavoriteBoard.java
+++ src/main/java/reuse/domain/FavoriteBoard.java
@@ -1,5 +1,6 @@
 package reuse.domain;
 
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -18,8 +19,24 @@ public class FavoriteBoard extends AbstractEntity {
     @JoinColumn(name = "board_id")
     private Board board;
 
+    @Builder
     public FavoriteBoard(User user, Board board) {
         this.user = user;
         this.board = board;
+    }
+
+    public static FavoriteBoard toEntity(Board board, User loginUser) {
+        return FavoriteBoard.builder()
+                .user(loginUser)
+                .board(board)
+                .build();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Board getBoard() {
+        return board;
     }
 }
