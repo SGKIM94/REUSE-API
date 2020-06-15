f132117ec7e12b1a3400b92395df25b9d7128e21
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-15 09:27:37 +0900

    [MODIFY] chat room 을 create 하는 mock API 를 제거한 후 product code 로 변경

--- src/main/java/reuse/web/ChatController.java
+++ src/main/java/reuse/web/ChatController.java
@@ -6,13 +6,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reuse.domain.User;
+import reuse.security.LoginUser;
+import reuse.service.ChatRoomService;
 
 @RestController
 @RequestMapping("/chats")
 public class ChatController {
+    private ChatRoomService chatRoomService;
+
+    public ChatController(ChatRoomService chatRoomService) {
+        this.chatRoomService = chatRoomService;
+    }
 
     @PostMapping
-    public ResponseEntity create(@RequestBody String name) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity create(@RequestBody String name, @LoginUser User loginUser) {
+        return ResponseEntity.ok().body(chatRoomService.create(name, loginUser));
     }
 }
