ea0c1bf07a7aa9f19655da007070f8e9c2e64509
sanggu <tkdrn8578@naver.com>
2020-06-16 22:59:09 +0900

    [MODIFY] chat room 을 상세 및 list 조회 시 given 에 조회를 하기 위한 채팅 방 생성 추가

--- src/test/java/reuse/chat/web/ChatAcceptanceTest.java
+++ src/test/java/reuse/chat/web/ChatAcceptanceTest.java
@@ -7,14 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import reuse.AbstractAcceptanceTest;
 import reuse.chat.domain.ChatRoom;
+import reuse.chat.dto.ListChatRoomsResponseView;
 import reuse.dto.board.CreateBoardResponseView;
 import reuse.security.TokenAuthenticationService;
 import reuse.web.CreateWebClientTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static reuse.chat.fixture.ChatFixture.TEST_CHAT_ROOM_NAME;
+import static reuse.chat.fixture.ChatFixture.*;
 import static reuse.fixture.CategoryFixture.CREATE_CATEGORY_REQUEST_VIEW;
-import static reuse.fixture.CommonFixture.DEFAULT_ID;
 
 public class ChatAcceptanceTest extends AbstractAcceptanceTest {
     public static final String CHAT_BASE_URL = "/chats";
@@ -48,10 +48,12 @@ public class ChatAcceptanceTest extends AbstractAcceptanceTest {
     @DisplayName("생성된 채팅방을 특정 ID 로 조회 가능한지")
     @Test
     public void findById() {
+        CreateBoardResponseView chatRoom = createChatRoom(TEST_CHAT_ROOM_NAME);
+
         //when
         EntityExchangeResult<ChatRoom> expectResponse
                 = restWebClientTest.getMethodWithAuthAcceptance
-                (CHAT_BASE_URL + "/" + DEFAULT_ID, ChatRoom.class, getJwt());
+                (CHAT_BASE_URL + "/" + chatRoom.getId(), ChatRoom.class, getJwt());
 
         //then
         HttpStatus status = expectResponse.getStatus();
@@ -61,14 +63,27 @@ public class ChatAcceptanceTest extends AbstractAcceptanceTest {
     @DisplayName("생성된 채팅방을 모두 조회 가능한지")
     @Test
     public void findAll() {
+        createChatRoom(TEST_CHAT_ROOM_NAME);
+        createChatRoom(TEST_SECOND_CHAT_ROOM_NAME);
+        createChatRoom(TEST_THIRD_CHAT_ROOM_NAME);
+
         //when
-        EntityExchangeResult<ChatRoom> expectResponse
+        EntityExchangeResult<ListChatRoomsResponseView> expectResponse
                 = restWebClientTest.getMethodWithAuthAcceptance
-                (CHAT_BASE_URL, ChatRoom.class, getJwt());
+                (CHAT_BASE_URL, ListChatRoomsResponseView.class, getJwt());
 
         //then
         HttpStatus status = expectResponse.getStatus();
+        ListChatRoomsResponseView responseBody = expectResponse.getResponseBody();
+
         assertThat(status).isEqualByComparingTo(HttpStatus.OK);
+        assertThat(responseBody.getSize()).isGreaterThan(3);
+    }
+
+    public CreateBoardResponseView createChatRoom(String name) {
+        return restWebClientTest.postMethodWithAuthAcceptance
+                (CHAT_BASE_URL, name, CreateBoardResponseView.class, getJwt())
+                .getResponseBody();
     }
 
     public String getJwt() {
