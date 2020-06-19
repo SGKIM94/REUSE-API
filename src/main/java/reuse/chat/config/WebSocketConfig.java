445d2cd257851f5cff9892b77a4ecf2645f86a1d
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-19 09:23:03 +0900

    [ADD] preSend 를 사용하기 위해서 WebSocketConfig 에 등록하는 configureClientInboundChannel 메서드 추가

--- src/main/java/reuse/chat/config/WebSocketConfig.java
+++ src/main/java/reuse/chat/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package reuse.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +10,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final StompHandler stompHandler;
+
+    public WebSocketConfig(StompHandler stompHandler) {
+        this.stompHandler = stompHandler;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chats/").setAllowedOrigins("*").withSockJS();
@@ -18,5 +25,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
