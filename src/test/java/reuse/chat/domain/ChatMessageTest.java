cebb11d3f15f4bf3e2f6d91f02d4549767bc5a67
sanggu <tkdrn8578@naver.com>
2020-06-20 14:27:08 +0900

    [ADD] 채팅 메시지의 type 이 join 인지에 대한 method 의 test code 를 추가

--- src/test/java/reuse/chat/domain/ChatMessageTest.java
+++ src/test/java/reuse/chat/domain/ChatMessageTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static reuse.chat.fixture.ChatFixture.TEST_CHAT_MESSAGE;
 import static reuse.chat.fixture.ChatFixture.TEST_JOIN_CHAT_MESSAGE;
 
 public class ChatMessageTest {
@@ -20,4 +21,25 @@ public class ChatMessageTest {
         //then
         assertThat(joinChatMessage.getMessage()).isEqualTo("김상구님이 입장하였습니다.");
     }
+
+    @DisplayName("입장 메시지인 경우 참을 리턴하는지")
+    @Test
+    public void isJoinMessageType() {
+        //when
+        boolean messageType = TEST_JOIN_CHAT_MESSAGE.isJoinMessageType();
+
+        //then
+        assertThat(messageType).isTrue();
+    }
+
+    @DisplayName("입장 메시지가 아닌 경우 거짓을 리턴하는지")
+    @Test
+    public void isNotJoinMessageType() {
+        //when
+        boolean messageType = TEST_CHAT_MESSAGE.isJoinMessageType();
+
+        //then
+        assertThat(messageType).isFalse();
+    }
+
 }
