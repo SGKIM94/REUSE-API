82508d93ecbc75e9fa95f68192364dc9306cc477
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-22 17:54:17 +0900

    [ADD] CONNECT 상태일 때 토큰이 verify 한지에 대한 method 추가

--- src/main/java/reuse/chat/config/StompHandler.java
+++ src/main/java/reuse/chat/config/StompHandler.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
+import reuse.exception.InvalidAccessTokenException;
 import reuse.security.TokenAuthenticationService;
 
 @Slf4j
@@ -22,10 +23,15 @@ public class StompHandler implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        if (StompCommand.CONNECT == accessor.getCommand()) {
-            tokenAuthenticationService.isVerifyToken(accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION));
+        if (isInvalidAccessTokenWhenConnect(accessor.getCommand(), accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION))) {
+            throw new InvalidAccessTokenException("Not invalid access token when connect websocket!");
         }
 
         return message;
+    }
+
+    boolean isInvalidAccessTokenWhenConnect(StompCommand accessorStatus, String jwt) {
+        return StompCommand.CONNECT == accessorStatus
+                && tokenAuthenticationService.isVerifyToken(jwt);
     }
 }
