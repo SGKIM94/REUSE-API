bfa6de4641124aecc89bc65c46bfc2e0165d8a43
sanggu <tkdrn8578@naver.com>
2020-06-23 23:29:53 +0900

    [ADD] BuyerReview 에 대한 repository 추가

---
+++ src/main/java/reuse/repository/BuyerReviewRepository.java
@@ -0,0 +1,7 @@
+package reuse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import reuse.domain.BuyerReview;
+
+public interface BuyerReviewRepository extends JpaRepository<BuyerReview, Long> {
+}
