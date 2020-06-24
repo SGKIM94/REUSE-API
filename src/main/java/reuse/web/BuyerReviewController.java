36dd02c7073aebcc38bf37eb093fe79cc6ee400e
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-24 18:35:52 +0900

    [ADD] buyerReview 를 생성하는 create mock 을 제거하고 product code 로 변경

--- src/main/java/reuse/web/BuyerReviewController.java
+++ src/main/java/reuse/web/BuyerReviewController.java
@@ -6,16 +6,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reuse.domain.BuyerReview;
 import reuse.domain.User;
-import reuse.dto.board.CreateBoardRequestView;
+import reuse.dto.review.buyer.CreateBuyerReviewRequestView;
 import reuse.security.LoginUser;
+import reuse.service.BuyerReviewService;
 
 @RestController
 @RequestMapping("/review/buyer")
 public class BuyerReviewController {
+    private BuyerReviewService buyerReviewService;
+
+    public BuyerReviewController(BuyerReviewService buyerReviewService) {
+        this.buyerReviewService = buyerReviewService;
+    }
 
     @PostMapping("")
-    public ResponseEntity create(@RequestBody CreateBoardRequestView board, @LoginUser User buyer) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity create(@RequestBody CreateBuyerReviewRequestView board, @LoginUser User buyer) {
+        BuyerReview buyerReview = buyerReviewService.create(board, buyer);
+        return ResponseEntity.ok().body(buyerReview.getId());
     }
 }
