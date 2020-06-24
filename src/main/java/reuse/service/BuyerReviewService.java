e828b5e18264121d1d4e11d169e2cbd6980eff3c
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-24 18:35:10 +0900

    [ADD] buyerReview 를 생성하는 service code 추가

---
+++ src/main/java/reuse/service/BuyerReviewService.java
@@ -0,0 +1,28 @@
+package reuse.service;
+
+import org.springframework.stereotype.Service;
+import reuse.domain.Board;
+import reuse.domain.BuyerReview;
+import reuse.domain.User;
+import reuse.dto.review.buyer.CreateBuyerReviewRequestView;
+import reuse.repository.BuyerReviewRepository;
+
+@Service
+public class BuyerReviewService {
+    private BuyerReviewRepository buyerReviewRepository;
+    private BoardService boardService;
+
+    public BuyerReviewService(BuyerReviewRepository buyerReviewRepository, BoardService boardService) {
+        this.buyerReviewRepository = buyerReviewRepository;
+        this.boardService = boardService;
+    }
+
+    public BuyerReview create(CreateBuyerReviewRequestView buyerReview, User buyer) {
+        BuyerReview savedBuyerReview = buyerReviewRepository.save(buyerReview.toEntity(buyer));
+
+        Board board = boardService.findById(buyerReview.getBoardId());
+        board.mappingBuyerReview(savedBuyerReview);
+
+        return buyerReviewRepository.save(buyerReview.toEntity(buyer));
+    }
+}
