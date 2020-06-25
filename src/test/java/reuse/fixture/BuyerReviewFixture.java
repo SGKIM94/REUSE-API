83c6bceb5180ee96a5d49411ee000ac7dbeab188
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-25 09:06:49 +0900

    [ADD] BuyerReview 를 테스트하기 위한 fixture 추가

--- src/test/java/reuse/fixture/BuyerReviewFixture.java
+++ src/test/java/reuse/fixture/BuyerReviewFixture.java
@@ -1,6 +1,9 @@
 package reuse.fixture;
 
+import reuse.domain.BuyerReview;
 import reuse.dto.review.buyer.CreateBuyerReviewRequestView;
+
+import static reuse.fixture.UserFixture.TEST_USER;
 
 public class BuyerReviewFixture extends CommonFixture {
     public static final String TEST_CONTENT = "리뷰입니다.";
@@ -10,6 +13,8 @@ public class BuyerReviewFixture extends CommonFixture {
     public static final CreateBuyerReviewRequestView CREATE_BUYER_REVIEW_REQUEST_VIEW = CreateBuyerReviewRequestView.builder()
             .content(TEST_CONTENT).rating(TEST_RATING).title(TEST_TITLE).boardId(DEFAULT_ID).build();
 
+    public static final BuyerReview TEST_BUYER_REVIEW = BuyerReview.builder()
+            .content(TEST_CONTENT).rating(TEST_RATING).title(TEST_TITLE).buyer(TEST_USER).build();
 
     public static CreateBuyerReviewRequestView getCreateBuyerReviewRequestView(Long boardId) {
         return CreateBuyerReviewRequestView.builder()
