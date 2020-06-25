6a0a0b8544bfa68b201069cf1cf5a88b1d53a888
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-25 09:07:16 +0900

    [ADD] BuyerReview 를 board 에서 mapping 시키는 것에 대한 test code 추가

--- src/test/java/reuse/domain/BoardTest.java
+++ src/test/java/reuse/domain/BoardTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static reuse.fixture.BoardFixture.*;
+import static reuse.fixture.BuyerReviewFixture.TEST_BUYER_REVIEW;
 
 public class BoardTest {
 
@@ -32,6 +34,31 @@ public class BoardTest {
 
         //then
         assertThat(board.getIsDeleted()).isTrue();
+    }
+
+    @DisplayName("Board 에 BuyerReview 를 연결시키기위한 외래키를 저장하는지")
+    @Test
+    public void mappingBuyerReview() {
+        //given
+        BuyerReview buyerReview = TEST_BUYER_REVIEW;
+        Board board = TEST_BOARD;
+
+        //when
+        board.mappingBuyerReview(buyerReview);
+
+        //then
+        assertThat(board.getBuyerReviewId()).isEqualTo(buyerReview.getId());
+    }
+
+    @DisplayName("Board 에 BuyerReview 를 연결시키기위한 외래키를 저장 시 존재하지 않을 경우 예외처리하는지")
+    @Test
+    public void mappingBuyerReviewFail() {
+        //when
+        String errorMessage = assertThrows(IllegalArgumentException.class, () -> {
+            TEST_BOARD.mappingBuyerReview(null);
+        }).getMessage();
+
+        assertThat(errorMessage).isEqualTo("Empty buyerReview when mapping to the Board!");
     }
 }
 
