c6faf3d8951d0aa13f07858fcebf447cddfbf010
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-26 17:29:48 +0900

    [ADD] 판매자에 연결되어 있는 게시글 후기를 모두 가져오는지에 대한 service test code 추가

--- src/test/java/reuse/repository/BuyerReviewRepositoryTest.java
+++ src/test/java/reuse/repository/BuyerReviewRepositoryTest.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reuse.dto.review.buyer.ListBuyerReviewRequestView;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static reuse.fixture.CommonFixture.DEFAULT_ID;
 
 @ExtendWith(SpringExtension.class)
@@ -16,11 +18,13 @@ public class BuyerReviewRepositoryTest {
     @Autowired
     private BuyerReviewRepository buyerReviewRepository;
 
-    @DisplayName("판매자에 연결되어 있는 게시글 후기를 가져오는지")
+    @DisplayName("판매자에 연결되어 있는 게시글 후기를 모두 가져오는지")
     @Test
-    @Sql(scripts = "/before-favorite-board.sql")
+    @Sql(scripts = {"/before-favorite-board.sql", "/insert-buyer-review.sql"})
     public void findBySeller() {
         //when
-         buyerReviewRepository.findBySeller(DEFAULT_ID);
+        ListBuyerReviewRequestView reviews = buyerReviewRepository.findBySeller(DEFAULT_ID);
+
+        assertThat(reviews.getSize()).isEqualTo(3);
     }
 }
