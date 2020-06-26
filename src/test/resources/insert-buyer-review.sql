fc15e24d4a6f06d4af17b7d6fadb44b8bcfd0a0b
kimsanggu <tkdrn8578@paymint.co.kr>
2020-06-26 17:29:25 +0900

    [ADD] buyer-review 를 db 에 저장하기 위한 query 추가

---
+++ src/test/resources/insert-buyer-review.sql
@@ -0,0 +1,12 @@
+INSERT INTO buyer_review
+(buyer_id, rating, title, content, create_at, update_at) VALUES (1, '10', '첫번 째 라뷰', '첫번 째 라뷰 내용', now(), now());
+
+INSERT INTO buyer_review
+(buyer_id, rating, title, content, create_at, update_at) VALUES (1, '5', '두번 째 라뷰', '두번 째 라뷰 내용', now(), now());
+
+INSERT INTO buyer_review
+(buyer_id, rating, title, content, create_at, update_at) VALUES (1, '7', '세번 째 라뷰', '세번 째 라뷰 내용', now(), now());
+
+UPDATE board SET buyer_review_id = 1 WHERE id = '1';
+UPDATE board SET buyer_review_id = 2 WHERE id = '2';
+UPDATE board SET buyer_review_id = 3 WHERE id = '3';
