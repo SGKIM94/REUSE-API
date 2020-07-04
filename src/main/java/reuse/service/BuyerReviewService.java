b6a9da4d06a5ff18a384c207cde07d65d739de15
sanggu <tkdrn8578@naver.com>
2020-07-04 17:14:01 +0900

    [ADD] 사용자와 구매자를 검증하는 verifyBuyer method 와 verifyBoardStatus method 추가

--- src/main/java/reuse/service/BuyerReviewService.java
+++ src/main/java/reuse/service/BuyerReviewService.java
@@ -1,5 +1,6 @@
 package reuse.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reuse.domain.Board;
@@ -10,6 +11,7 @@ import reuse.dto.review.buyer.ListBuyerReviewResponseView;
 import reuse.repository.BuyerReviewRepository;
 
 @Service
+@Slf4j
 public class BuyerReviewService {
     private BuyerReviewRepository buyerReviewRepository;
     private BoardService boardService;
@@ -27,6 +29,24 @@ public class BuyerReviewService {
         board.mappingBuyerReview(savedBuyerReview);
 
         return buyerReviewRepository.save(buyerReview.toEntity(buyer));
+    }
+
+    public Board verifyBuyer(CreateBuyerReviewRequestView buyerReview, User buyer) {
+        Board board = boardService.findById(buyerReview.getBoardId());
+
+        if (board.getBuyerId() != buyer.getId()) {
+            log.error("구매자의 ID : " + board.getBuyerId());
+            log.error("로그인 사용자의 ID : " + buyer.getId());
+            throw new IllegalArgumentException("해당 게시글의 구매자와 로그인 사용자와 일치하지 않습니다.");
+        }
+
+        return board;
+    }
+
+    public void verifyBoardStatus(Board board) {
+        if (board.getSalesStatus() == null || !board.isCompleteSalesStatus()) {
+            throw new IllegalArgumentException("거래 완료 상태가 아니기 때문에 후기를 작성할 수 없습니다. : " + board.getSalesStatus());
+        }
     }
 
     public ListBuyerReviewResponseView findBySeller(Long sellerId) {
