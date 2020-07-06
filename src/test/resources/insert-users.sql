ea50c1a1ffee0e62ff45a146bea4407590436211
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-06 17:44:33 +0900

    [ADD] insert-user 에 SECOND USER 에 대한 정보 추가

--- src/test/resources/insert-users.sql
+++ src/test/resources/insert-users.sql
@@ -1,2 +1,5 @@
 INSERT INTO user (id, create_at, update_at, name, social_token_id, social_type)
 VALUES ('1', '2020-05-29 10:25:49', '2020-05-29 10:25:49', '홍길동', 'toeknid', 'NAVER');
+
+INSERT INTO user (id, create_at, update_at, name, social_token_id, social_type)
+VALUES ('2', '2020-05-29 10:25:49', '2020-05-29 10:25:49', '투길동', 'secondTokenId', 'KAKAO');
