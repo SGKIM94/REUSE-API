ae639f9d01e33f3405258a70a56e232497096851
sanggu <tkdrn8578@naver.com>
2020-07-14 23:41:40 +0900

    [ADD] Seller review 를 create 하는 것에 대한 DTO 추가

---
+++ src/main/java/reuse/dto/review/seller/CreateSellerReviewRequestView.java
@@ -0,0 +1,27 @@
+package reuse.dto.review.seller;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import reuse.domain.User;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateSellerReviewRequestView {
+    private String title;
+    private String content;
+    private String rating;
+    private User seller;
+    private Long boardId;
+
+    @Builder
+    public CreateSellerReviewRequestView(String title, String content, String rating, Long boardId, User seller) {
+        this.title = title;
+        this.content = content;
+        this.rating = rating;
+        this.boardId = boardId;
+        this.seller = seller;
+    }
+}
