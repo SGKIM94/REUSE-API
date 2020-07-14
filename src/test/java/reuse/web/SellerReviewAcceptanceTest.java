6257edb4811e22ce8e72989f69ae2511654862d4
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-14 18:37:51 +0900

    [ADD] 판매 후기에 대한 Acceptanace test code 추가

---
+++ src/test/java/reuse/web/SellerReviewAcceptanceTest.java
@@ -0,0 +1,64 @@
+package reuse.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import reuse.AbstractAcceptanceTest;
+import reuse.domain.Board;
+import reuse.domain.BuyerReview;
+import reuse.dto.board.CreateBoardResponseView;
+import reuse.security.TokenAuthenticationService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static reuse.fixture.BoardFixture.CREATE_BOARD_REQUEST_VIEW;
+import static reuse.fixture.BuyerReviewFixture.getCreateBuyerReviewRequestView;
+import static reuse.web.BoardAcceptanceTest.BOARD_BASE_URL;
+
+public class SellerReviewAcceptanceTest extends AbstractAcceptanceTest {
+    public static final String BUYER_REVIEW_BASE_URL = "/review/buyer";
+
+    private CreateWebClientTest createWebClientTest;
+    private TokenAuthenticationService tokenAuthenticationService;
+    private String socialTokenId;
+
+    @BeforeEach
+    void setUp() {
+        this.createWebClientTest = new CreateWebClientTest(this.webTestClient);
+        this.tokenAuthenticationService = new TokenAuthenticationService();
+        socialTokenId = createWebClientTest.createUser();
+    }
+
+    @DisplayName("판매자가 구매한 게시판에 구매평을 남기는지")
+    @Test
+    @Sql(scripts = {"/clean-all.sql", "/insert-categories.sql", "/insert-products.sql"})
+    public void createBuyerReview() {
+        //given
+        CreateBoardResponseView board = createWebClientTest.createBoard(CREATE_BOARD_REQUEST_VIEW, getJwt());
+        Long boardId = board.getId();
+
+        //when
+        EntityExchangeResult<BuyerReview> expectResponse
+                = createWebClientTest.postMethodWithAuthAcceptance
+                (BUYER_REVIEW_BASE_URL, getCreateBuyerReviewRequestView(boardId), BuyerReview.class, getJwt());
+
+        //then
+        Board foundBoard = findBoardById(boardId);
+
+        HttpStatus status = expectResponse.getStatus();
+
+        assertThat(status).isEqualTo(HttpStatus.OK);
+        assertThat(foundBoard.getBuyerReview()).isNotNull();
+    }
+
+    public Board findBoardById(Long boardId) {
+        return createWebClientTest.getMethodWithAuthAcceptance(BOARD_BASE_URL + "/" + boardId, Board.class, getJwt())
+                .getResponseBody();
+    }
+
+    public String getJwt() {
+        return tokenAuthenticationService.toJwtBySocialTokenId(socialTokenId);
+    }
+}
