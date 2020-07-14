28491561c4afa5bcca4e9ee126d96a2a8217a10a
sanggu <tkdrn8578@naver.com>
2020-07-14 23:41:17 +0900

    [ADD] 판매자가 구매자에 대한 후기를 남길 수 있도록 생성하기 위한 mock API 추가

---
+++ src/main/java/reuse/web/SellerReviewController.java
@@ -0,0 +1,20 @@
+package reuse.web;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reuse.domain.User;
+import reuse.dto.review.seller.CreateSellerReviewRequestView;
+import reuse.security.LoginUser;
+
+@RestController
+@RequestMapping("/review/buyer")
+public class SellerReviewController {
+    @PostMapping("")
+    public ResponseEntity create(@RequestBody CreateSellerReviewRequestView review, @LoginUser User buyer) {
+        return ResponseEntity.ok().build();
+    }
+}
