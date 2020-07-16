c7d80a91e7eee31262b6dc2ac3604a9e743b3513
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-16 18:31:38 +0900

    [ADD] seller review 를 작성하는 create API 로직 추가

--- src/main/java/reuse/web/SellerReviewController.java
+++ src/main/java/reuse/web/SellerReviewController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import reuse.domain.User;
 import reuse.dto.review.seller.CreateSellerReviewRequestView;
 import reuse.security.LoginUser;
+import reuse.service.SellerReviewService;
 
 @RestController
 @RequestMapping("/review/buyer")
 public class SellerReviewController {
+    private SellerReviewService sellerReviewService;
 
     @PostMapping
-    public ResponseEntity create(@RequestBody CreateSellerReviewRequestView review, @LoginUser User buyer) {
+    public ResponseEntity create(@RequestBody CreateSellerReviewRequestView review, @LoginUser User seller) {
+        sellerReviewService.create(review, seller);
         return ResponseEntity.ok().build();
     }
 }
