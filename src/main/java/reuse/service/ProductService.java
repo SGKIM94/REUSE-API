71fb427164faa0eafb21ebc4775ffdf924defb75
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-21 18:41:59 +0900

    [MODIFY] product images 를 list 로 변경하는 것과 tumbnail 제거에 따른 로직 수정

--- src/main/java/reuse/service/ProductService.java
+++ src/main/java/reuse/service/ProductService.java
@@ -5,7 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import reuse.domain.Product;
 import reuse.domain.ProductImages;
-import reuse.dto.product.*;
+import reuse.dto.product.CreateProductRequestView;
+import reuse.dto.product.CreateProductResponseView;
+import reuse.dto.product.FindProductResponseView;
+import reuse.dto.product.ListProductResponseView;
 import reuse.repository.ProductImagesRepository;
 import reuse.repository.ProductRepository;
 import reuse.storage.S3Uploader;
@@ -18,7 +21,6 @@ import java.util.stream.Collectors;
 @Service
 public class ProductService {
     public static final String S3_PRODUCT_IMAGES_DIRECTORY_NAME = "products/";
-    public static final String THUMBNAIL_DIRECTORY = "/thumbnail";
 
     private final ProductRepository productRepository;
     private final ProductImagesRepository productImagesRepository;
@@ -34,11 +36,10 @@ public class ProductService {
     public CreateProductResponseView create(CreateProductRequestView product) {
         String imageDirectory = getImageDirectory();
 
-        String imageUrl = storeThumbnailImage(product, imageDirectory);
         List<String> imagesUrl = storeProductImages(product, imageDirectory);
 
         productImagesRepository.save(ProductImages.toEntity(imagesUrl));
-        Product savedProduct = productRepository.save(product.toEntity(product, imageUrl, imagesUrl));
+        Product savedProduct = productRepository.save(product.toEntity(product, imagesUrl));
 
         return CreateProductResponseView.toDto(savedProduct);
     }
@@ -57,22 +58,17 @@ public class ProductService {
         return FindProductResponseView.toDto(findById(id));
     }
 
-    String storeThumbnailImage(CreateProductRequestView product, String directory) {
-        MultipartFile productImage = product.getThumbnailImage();
-        return storeProductImage(productImage, directory + THUMBNAIL_DIRECTORY);
-    }
-
     public List<String> storeProductImages(CreateProductRequestView product, String directory) {
-        ProductImagesView productImages = ProductImagesView.toDtoByCreate(product);
-        if (productImages == null) {
+        List<MultipartFile> images = product.getImages();
+        if (images == null) {
             return new ArrayList<>();
         }
 
-        return storeProductImageByProductImagesView(productImages, directory);
+        return storeProductImageByProductImagesView(images, directory);
     }
 
-    public List<String> storeProductImageByProductImagesView(ProductImagesView productImages, String directory) {
-        return productImages.convertToList().stream()
+    public List<String> storeProductImageByProductImagesView(List<MultipartFile> productImages, String directory) {
+        return productImages.stream()
                 .map(image -> storeProductImage(image, directory))
                 .collect(Collectors.toList());
     }
