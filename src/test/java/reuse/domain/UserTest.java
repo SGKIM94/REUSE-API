04ebcd03b5df24b66e7571b13f402d006884dd17
sanggu <tkdrn8578@naver.com>
2020-07-23 23:12:09 +0900

    [ADD] 사용자에 대한 점수를 더하는 것에 대한 테스트 코드 추가

--- src/test/java/reuse/domain/UserTest.java
+++ src/test/java/reuse/domain/UserTest.java
@@ -1,7 +1,20 @@
 package reuse.domain;
 
-public class UserTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static reuse.fixture.UserFixture.TEST_USER;
+
+public class UserTest {
+    @DisplayName("후기에 작성된 평가 점수가 사용자에게 더해지는지")
+    @Test
+    public void addScore() {
+        int score = 10;
+
+        int addedScore = TEST_USER.addScore(score);
+        assertThat(addedScore).isEqualTo(10);
+    }
 }
 
 
