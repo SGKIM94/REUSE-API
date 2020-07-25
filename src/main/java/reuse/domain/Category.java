670853422541feb4b2f8260655c795a279751598
sanggu <tkdrn8578@naver.com>
2020-07-25 14:29:17 +0900

    [ADD] Category domain 에 deviceChange 컬럼 데이터 추가

--- src/main/java/reuse/domain/Category.java
+++ src/main/java/reuse/domain/Category.java
@@ -16,17 +16,21 @@ public class Category extends AbstractEntity {
 
     private String model;
 
+    private String deviceChange;
+
     @Builder
-    public Category(String teleco, String manufacturer, String model) {
+    public Category(String teleco, String manufacturer, String model, String deviceChange) {
         this.teleco = teleco;
         this.manufacturer = manufacturer;
         this.model = model;
+        this.deviceChange = deviceChange;
     }
 
-    public Category(Long id, String teleco, String manufacturer, String model) {
+    public Category(Long id, String teleco, String manufacturer, String model, String deviceChange) {
         super(id);
         this.teleco = teleco;
         this.manufacturer = manufacturer;
         this.model = model;
+        this.deviceChange = deviceChange;
     }
 }
