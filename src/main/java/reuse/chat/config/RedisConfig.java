fe2160ccd197a05664e685e29039211e3deb1bfc
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-27 09:05:55 +0900

    [ADD] redis 의 pub/sub 기능을 사용하기 위해 MessageListener 설정을 추가 및 redisTemplate 설정 추가

---
+++ src/main/java/reuse/chat/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package reuse.chat.config;
+
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+public class RedisConfig {
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+
+        return redisTemplate;
+    }
+}
