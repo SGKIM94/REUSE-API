787ff8824c271fa4a77f4387ee03ef080432a716
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-27 18:16:44 +0900

    [ADD] Redis 를 이용하여 메시지를 pub 하기 위한 service 추가

---
+++ src/main/java/reuse/chat/service/RedisPublisher.java
@@ -0,0 +1,19 @@
+package reuse.chat.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate redisTemplate;
+
+    public void publish(ChannelTopic topic, Message message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
