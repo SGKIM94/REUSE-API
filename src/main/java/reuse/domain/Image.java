155180230c3671372522bf790f0f1945043f22ef
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-28 17:48:38 +0900

    [ADD] Product 의 image 를 일대 다로 관리하기 위한 Image Entity 추가
     * 양방향이 연관관계를 확인하기 좋고, 삭제할 때 성능 문제로 인한 연결

---
+++ src/main/java/reuse/domain/Image.java
@@ -0,0 +1,32 @@
+package reuse.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Image extends AbstractEntity {
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+
+    private String url;
+
+    @Builder
+    public Image(Product product, String url) {
+        this.product = product;
+        this.url = url;
+    }
+
+    public Image(long id, Product product, String url) {
+        super(id);
+        this.product = product;
+        this.url = url;
+    }
+}
