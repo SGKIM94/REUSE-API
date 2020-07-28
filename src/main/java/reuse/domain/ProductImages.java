6e681e9994a6a9a12bf4eb81bda35372dbf52a2e
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-28 17:49:27 +0900

    [MODIFY] ProductImage 를 Image 를 List 된 것을 일급콜렉션으로 감싸 Embed 하도록 변경

--- src/main/java/reuse/domain/ProductImages.java
+++ src/main/java/reuse/domain/ProductImages.java
@@ -1,49 +1,41 @@
 package reuse.domain;
 
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Entity
+@Embeddable
 @NoArgsConstructor
-@Getter
-public class ProductImages extends AbstractEntity {
-    private String firstImage;
-
-    private String secondImage;
-
-    private String thirdImage;
-
-    private String fourthImage;
-
-    private String fifthImage;
-
-    private String sixthImage;
+public class ProductImages {
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "image_id")
+    private List<Image> images;
 
 
     @Builder
-    public ProductImages(String firstImage, String secondImage, String thirdImage, String fourthImage,
-                         String fifthImage, String sixthImage) {
-        this.firstImage = firstImage;
-        this.secondImage = secondImage;
-        this.thirdImage = thirdImage;
-        this.fourthImage = fourthImage;
-        this.fifthImage = fifthImage;
-        this.sixthImage = sixthImage;
+    public ProductImages(List<Image> images) {
+        this.images = images;
     }
 
-    public static ProductImages toEntity(List<String> imageUrls) {
-        //TODO: 상품 이미지가 5개 이하일 경우에 대한 오류 처리 필요
-        return ProductImages.builder()
-                .firstImage(imageUrls.get(0))
-                .secondImage(imageUrls.get(1))
-                .thirdImage(imageUrls.get(2))
-                .fourthImage(imageUrls.get(3))
-                .fifthImage(imageUrls.get(4))
-                .sixthImage(imageUrls.get(5))
-                .build();
+    public static ProductImages toEntity(List<String> imageUrls, Product savedProduct) {
+        List<Image> images = imageUrls.stream()
+                .map(url -> new Image(savedProduct, url))
+                .collect(Collectors.toList());
+
+        return new ProductImages(images);
+    }
+
+    public int getSize() {
+        return this.images.size();
+    }
+
+    public String getIndexImage(int index) {
+        return this.images.get(index).getUrl();
     }
 }
