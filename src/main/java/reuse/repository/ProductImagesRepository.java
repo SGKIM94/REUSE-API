614d592bc21888a1230607fea6e7b0cc22e893ad
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-29 16:33:09 +0900

    [MODIFY] Image 추가에 따른 ProductImages 제거

--- src/main/java/reuse/repository/ProductImagesRepository.java
+++
@@ -1,7 +0,0 @@
-package reuse.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import reuse.domain.ProductImages;
-
-public interface ProductImagesRepository extends JpaRepository<ProductImages, Long> {
-}
