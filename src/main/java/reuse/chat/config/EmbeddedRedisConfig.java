5401aa1bef0f38f84aaba86f7f2e5628cd435af7
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-29 18:38:22 +0900

    [MODIFY] redis 를 사용하지 않는 부분 임시 주석

--- src/main/java/reuse/chat/config/EmbeddedRedisConfig.java
+++ src/main/java/reuse/chat/config/EmbeddedRedisConfig.java
@@ -1,15 +1,13 @@
 package reuse.chat.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import redis.embedded.RedisServer;
 
 @Configuration
 public class EmbeddedRedisConfig {
-    private RedisServer redisServer;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
+//    private RedisServer redisServer;
+//
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
 
 //    @PostConstruct
 //    public void redisServer() {
