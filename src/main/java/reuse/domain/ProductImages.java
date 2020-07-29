ceb773151c14fb30eecc4bd6b0bf9c0269037980
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-29 16:42:22 +0900

    [ADD] product image 가 해당 index 가 존재하지 않을 때에 대한 오류 처리 추가

--- src/main/java/reuse/domain/ProductImages.java
+++ src/main/java/reuse/domain/ProductImages.java
@@ -2,6 +2,7 @@ package reuse.domain;
 
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
@@ -12,11 +13,11 @@ import java.util.stream.Collectors;
 
 @Embeddable
 @NoArgsConstructor
+@Slf4j
 public class ProductImages {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "image_id")
     private List<Image> images;
-
 
     @Builder
     public ProductImages(List<Image> images) {
@@ -36,6 +37,11 @@ public class ProductImages {
     }
 
     public String getIndexImage(int index) {
+        if (this.images.get(index) == null) {
+            log.error("해당 Images : " + images.toString());
+            throw new IllegalArgumentException("해당 Index 에 Product Image 가 존재하지 않습니다. : " + index);
+        }
+
         return this.images.get(index).getUrl();
     }
 }
