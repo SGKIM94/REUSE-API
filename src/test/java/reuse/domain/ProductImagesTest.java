8cafbcfd7600e3676f52990b538b40269746019d
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-29 16:42:06 +0900

    [ADD] image 가 해당 index 에 존재하지 않을 때 예외를 처리하는지에 대한 test code 추가

--- src/test/java/reuse/domain/ProductImagesTest.java
+++ src/test/java/reuse/domain/ProductImagesTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static reuse.fixture.ProductFixture.TEST_PRODUCT;
 import static reuse.fixture.ProductImagesFixture.TEST_IMAGE_URLS;
+import static reuse.fixture.ProductImagesFixture.TEST_PRODUCT_IMAGES;
 
 public class ProductImagesTest {
     @DisplayName("이미지에 대한 URL 들과 품목으로 ProductImages 를 리턴하는지")
@@ -15,6 +17,14 @@ public class ProductImagesTest {
 
         assertThat(productImages.getSize()).isEqualTo(6);
     }
+
+    @DisplayName("product image ")
+    @Test
+    public void getIndexImage() {
+        String message = assertThrows(IllegalArgumentException.class, () -> {
+            TEST_PRODUCT_IMAGES.getIndexImage(7);
+        }).getMessage();
+
+        assertThat(message).isEqualTo("해당 Index 에 Product Image 가 존재하지 않습니다. : 7");
+    }
 }
-
-
