db1518fa8698d0c2ff03749ad98b0f0dcafc57c1
kimsanggu <tkdrn8578@paymint.co.kr>
2020-07-30 14:08:30 +0900

    [MODIFY] board, product, image 에 대한 컬럼 수정에 따른 test 를 위한 query file 수정

--- src/test/resources/before-favorite-board.sql
+++ src/test/resources/before-favorite-board.sql
@@ -1,11 +1,13 @@
 -- clean all
 DELETE FROM favorite_board WHERE id IS NOT NULL;
 DELETE FROM board WHERE id IS NOT NULL;
+DELETE FROM image WHERE id IS NOT NULL;
 DELETE FROM product WHERE id IS NOT NULL;
-DELETE FROM product_images WHERE id IS NOT NULL;
 DELETE FROM category WHERE id IS NOT NULL;
 DELETE FROM chat_room WHERE id IS NOT NULL;
 DELETE FROM buyer_review WHERE id IS NOT NULL;
+DELETE FROM seller_review WHERE id IS NOT NULL;
+
 -- clean user
 DELETE FROM user WHERE id IS NOT NULL;
 
@@ -39,51 +41,66 @@ INSERT INTO category
 VALUES ('6', '2020-06-03 09:22:12', '2020-06-03 09:22:12', '1', '2', '2');
 
 -- insert products and images
-INSERT INTO product (id, explanation, is_sold, is_used, name, price, tax, thumbnail_image, create_at, update_at)
-VALUES ('9999999998', '테스트 설명', true, false, '테스트 품목', '20000', '1000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T10%3A41%3A31.267/thumbnail/1590716298286', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO product (id, explanation, is_sold, is_used, name, price, tax, create_at, update_at)
+VALUES ('9999999998', '테스트 설명', true, false, '테스트 품목', '20000', '1000', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
 
-INSERT INTO product (id, explanation, is_sold, is_used, name, price, tax, thumbnail_image, create_at, update_at)
-VALUES ('9999999999', '테스트 설명2', true, false, '테스트 품목2', '50000', '5000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T10%3A41%3A31.267/thumbnail/1590716298286', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
-
-INSERT INTO product_images (id, first_image, second_image, third_image, fourth_image, fifth_image, sixth_image, create_at, update_at)
-VALUES (    '1',
-            'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-            'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-            'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-            'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-            'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-            'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682/test-product-image',
-            '2020-05-29 10:25:49', '2020-05-29 10:25:49');
-
-INSERT INTO product_images (id, first_image, second_image, third_image, fourth_image, fifth_image, sixth_image, create_at, update_at)
-VALUES (
-           '2',
-           'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-           'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-           'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-           'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-           'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682',
-           'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682/test-product-image',
-           '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO product (id, explanation, is_sold, is_used, name, price, tax, create_at, update_at)
+VALUES ('9999999999', '테스트 설명2', true, false, '테스트 품목2', '50000', '5000', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
 
 
-INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, thumbnail_image, product_images_id, quality, create_at, update_at)
-VALUES ('1', '1', '첫번 째 상품', false, false, '첫번째', '1000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '1', '1', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('1', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999998', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
 
-INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, thumbnail_image, product_images_id, quality, create_at, update_at)
-VALUES ('2', '2', '두번 째 상품', false, false, '첫번째', '1000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '1', '1', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('2', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999998', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
 
-INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, thumbnail_image, product_images_id, quality, create_at, update_at)
-VALUES ('3', '3', '세번 째 상품', false, false, '첫번째', '1000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '1', '1', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('3', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999998', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
 
-INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, thumbnail_image, product_images_id, quality, create_at, update_at)
-VALUES ('4', '4', '네번 째 상품', false, false, '첫번째', '1000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '1', '1', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('4', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999998', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
 
-INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, thumbnail_image, product_images_id, quality, create_at, update_at)
-VALUES ('5', '5', '다섯번 째 상품', false, false, '첫번째', '1000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '1', '1', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('5', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999998', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
 
-INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, thumbnail_image, product_images_id, quality, create_at, update_at)
-VALUES ('6', '6', '다섯번 째 상품', false, false, '첫번째', '1000', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '1', '1', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('6', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999998', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
+
+
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('7', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999999', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
+
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('8', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999999', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
+
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('9', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999999', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
+
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('10', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999999', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
+
+INSERT INTO image (id, url, product_id, update_at, create_at)
+VALUES ('11', 'https://reuse-s3.s3.ap-northeast-2.amazonaws.com/products/2020-05-29T09%3A57%3A51.887/1590713808682', '9999999999', '2020-05-29 09:57:52', '2020-05-29 09:57:52');
+
+
+INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, quality, create_at, update_at)
+VALUES ('1', '1', '첫번 째 상품', false, false, '첫번째', '1000', 'S', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+
+INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, quality, create_at, update_at)
+VALUES ('2', '2', '두번 째 상품', false, false, '첫번째', '1000', 'S', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+
+INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, quality, create_at, update_at)
+VALUES ('3', '3', '세번 째 상품', false, false, '첫번째', '1000', 'S', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+
+INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, quality, create_at, update_at)
+VALUES ('4', '4', '네번 째 상품', false, false, '첫번째', '1000', 'S', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+
+INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, quality, create_at, update_at)
+VALUES ('5', '5', '다섯번 째 상품', false, false, '첫번째', '1000', 'S', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+
+INSERT INTO product (id, category_id, explanation, is_sold, is_used, name, price, quality, create_at, update_at)
+VALUES ('6', '6', '다섯번 째 상품', false, false, '첫번째', '1000', 'S', '2020-05-29 10:25:49', '2020-05-29 10:25:49');
+
 
 
 -- insert boards
