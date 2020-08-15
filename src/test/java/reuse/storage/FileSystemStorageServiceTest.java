248c3b7c2553613814c123f4f73247ba1a538fd6
sanggu <tkdrn8578@naver.com>
2020-08-15 16:33:38 +0900

    [MODIFY] 사용하지 않는 파일 Service TEST 에 대한 disabled 추가

--- src/test/java/reuse/storage/FileSystemStorageServiceTest.java
+++ src/test/java/reuse/storage/FileSystemStorageServiceTest.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static reuse.fixture.ProductImagesFixture.TEST_IMAGE1;
 
+@Disabled("파일 업로드 사용 S3 로 변경")
 public class FileSystemStorageServiceTest {
     public static final String REUSE_LOCATION = "/Users/kimsang-gu/Workspace/sanggu/toy-project/reuse/reuse-api/";
 
