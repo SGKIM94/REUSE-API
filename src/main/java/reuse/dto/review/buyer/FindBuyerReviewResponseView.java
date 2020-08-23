1d9a9e3a7bdd8b55d206e37a2f4a716f3233da29
sanggu <tkdrn8578@naver.com>
2020-08-23 14:34:27 +0900

    [MODIFY] BuyerReview 를 조회 시 LAZY 로 인해 데이터가 조회되지 않는 문제로 인해 DTO 생성

---
+++ src/main/java/reuse/dto/review/buyer/FindBuyerReviewResponseView.java
@@ -0,0 +1,38 @@
+package reuse.dto.review.buyer;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import reuse.domain.BuyerReview;
+import reuse.domain.User;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FindBuyerReviewResponseView {
+    private Long id;
+    private User buyer;
+    private Integer score;
+    private String title;
+    private String content;
+
+    @Builder
+    public FindBuyerReviewResponseView(Long id, User buyer, Integer score, String title, String content) {
+        this.id = id;
+        this.buyer = buyer;
+        this.score = score;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static FindBuyerReviewResponseView toDto(BuyerReview buyerReview) {
+        return FindBuyerReviewResponseView.builder()
+                .id(buyerReview.getId())
+                .buyer(buyerReview.getBuyer())
+                .score(buyerReview.getScore())
+                .title(buyerReview.getTitle())
+                .content(buyerReview.getContent())
+                .build();
+    }
+}
