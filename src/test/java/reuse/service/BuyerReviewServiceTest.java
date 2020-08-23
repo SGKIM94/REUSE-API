4b7d4ae9e21d3739121ea9aa9ccb91c767b70680
sanggu <tkdrn8578@naver.com>
2020-08-23 14:33:57 +0900

    [MODIFY] BuyerReview Service test 동시에 돌렸을 때 modify 로 인해 발생하는 오류를 순서를 지정함에 따른 수정

--- src/test/java/reuse/service/BuyerReviewServiceTest.java
+++ src/test/java/reuse/service/BuyerReviewServiceTest.java
@@ -5,6 +5,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import reuse.domain.BuyerReview;
 import reuse.domain.User;
+import reuse.dto.review.buyer.FindBuyerReviewResponseView;
 import reuse.dto.review.buyer.ListBuyerReviewResponseView;
 import reuse.repository.BuyerReviewRepository;
 
@@ -32,6 +33,7 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
 
     @DisplayName("게시물에 대한 후기가 생성되는지")
     @Test
+    @Order(1)
     public void create() {
         //given
         User buyer = TEST_USER;
@@ -43,12 +45,13 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
         BuyerReview buyerReview = buyerReviewService.create(CREATE_BUYER_REVIEW_REQUEST_VIEW, buyer);
 
         //then
-        assertThat(buyer.getScore()).isEqualTo(10);
+        assertThat(buyer.getScore()).isEqualTo(TEST_SCORE);
         assertThat(buyerReview.getId()).isNotNull();
     }
 
     @DisplayName("판매자에 연결되어 있는 게시글 후기를 모두 가져오는지")
     @Test
+    @Order(2)
     public void findBySeller() {
         //given
         when(buyerReviewRepository.findBySeller(any())).thenReturn(LIST_BUYER_REVIEW_REQUEST_VIEW);
@@ -62,13 +65,13 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
 
     @DisplayName("구매후기를 해당 ID 로 조회 가능한지")
     @Test
-    @Order(1)
-    public void retrieve() {
+    @Order(3)
+    public void findById() {
         //given
         when(buyerReviewRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BUYER_REVIEW));
 
         //when
-        BuyerReview buyerReview = buyerReviewService.retrieve(DEFAULT_ID);
+        BuyerReview buyerReview = buyerReviewService.findById(DEFAULT_ID);
 
         //then
         assertThat(buyerReview.getContent()).isEqualTo(TEST_CONTENT);
@@ -76,8 +79,26 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
         assertThat(buyerReview.getTitle()).isEqualTo(TEST_TITLE);
     }
 
+    @DisplayName("구매후기를 해당 ID 로 조회 후 DTO 로 변환하는지")
+    @Test
+    @Order(4)
+    public void retrieve() {
+        //given
+        when(buyerReviewRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BUYER_REVIEW));
+
+        //when
+        FindBuyerReviewResponseView buyerReview = buyerReviewService.retrieve(DEFAULT_ID);
+
+        //then
+        assertThat(buyerReview.getContent()).isEqualTo(TEST_CONTENT);
+        assertThat(buyerReview.getScore()).isEqualTo(TEST_SCORE);
+        assertThat(buyerReview.getTitle()).isEqualTo(TEST_TITLE);
+    }
+
+
     @DisplayName("모든 구매후기를 조회 가능한지")
     @Test
+    @Order(5)
     public void list() {
         //given
         when(buyerReviewRepository.findAll()).thenReturn(TEST_BUYER_REVIEWS);
@@ -91,7 +112,7 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
 
     @DisplayName("구매후기를 수정 가능한지")
     @Test
-    @Order(2)
+    @Order(6)
     public void modify() {
         //given
         when(buyerReviewRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BUYER_REVIEW));
@@ -105,6 +126,7 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
 
     @DisplayName("구매후기가 존재하지 않는 것을 조회 시 예외를 처리하는지")
     @Test
+    @Order(7)
     public void retrieveFail() {
         //when
         String errorMessage = assertThrows(IllegalArgumentException.class, () -> {
@@ -117,6 +139,7 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
 
     @DisplayName("구매후기를 작성하는 사용자와 게시글의 구매자와 같지 않은 경우 예외를 처리하는지")
     @Test
+    @Order(8)
     public void verifyBuyer() {
         //given
         when(boardService.findById(any())).thenReturn(TEST_BOARD);
@@ -131,6 +154,7 @@ public class BuyerReviewServiceTest extends AbstractServiceTest {
 
     @DisplayName("구매후기를 작성하는 사용자와 게시글이 거래 완료 상태가 아닌 경우 예외를 처리하는지")
     @Test
+    @Order(9)
     public void verifySalesStatus() {
         //when
         String errorMessage = assertThrows(IllegalArgumentException.class, () -> {
