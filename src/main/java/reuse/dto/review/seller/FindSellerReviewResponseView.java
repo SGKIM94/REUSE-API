7518a455ad35dfe00864bfaf1d312ba380447c69
sanggu <tkdrn8578@naver.com>
2020-08-25 23:26:51 +0900

    [ADD] sellerReview 를 findById 한 경우 리턴하기 위한 DTO 추가

---
+++ src/main/java/reuse/dto/review/seller/FindSellerReviewResponseView.java
@@ -0,0 +1,38 @@
+package reuse.dto.review.seller;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import reuse.domain.SellerReview;
+import reuse.domain.User;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FindSellerReviewResponseView {
+    private Long id;
+    private User seller;
+    private Integer score;
+    private String title;
+    private String content;
+
+    @Builder
+    public FindSellerReviewResponseView(Long id, User seller, Integer score, String title, String content) {
+        this.id = id;
+        this.seller = seller;
+        this.score = score;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static FindSellerReviewResponseView toDto(SellerReview sellerReview) {
+        return FindSellerReviewResponseView.builder()
+                .id(sellerReview.getId())
+                .seller(sellerReview.getSeller())
+                .score(sellerReview.getScore())
+                .title(sellerReview.getTitle())
+                .content(sellerReview.getContent())
+                .build();
+    }
+}
