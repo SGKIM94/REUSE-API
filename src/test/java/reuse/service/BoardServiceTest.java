0249c6f85b8d08b58ac310684f12ed9fe00878b8
sanggu <tkdrn8578@naver.com>
2020-08-26 21:53:24 +0900

    [MODIFY] fixture 가 modify 되면서 값이 변경되는 현상으로 인해 고립하는 방법을 찾기 전 우선 before 에서 새로 생성하는 형태로 변경 및 order 수정

--- src/test/java/reuse/service/BoardServiceTest.java
+++ src/test/java/reuse/service/BoardServiceTest.java
@@ -1,7 +1,6 @@
 package reuse.service;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import reuse.domain.Board;
@@ -21,8 +20,10 @@ import static org.mockito.Mockito.when;
 import static reuse.fixture.BoardFixture.*;
 import static reuse.fixture.ProductFixture.TEST_PRODUCT;
 import static reuse.fixture.ProductImagesFixture.FIRST_IMAGE_URL;
+import static reuse.fixture.UserFixture.TEST_SECOND_USER;
 import static reuse.fixture.UserFixture.TEST_USER;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BoardServiceTest extends AbstractServiceTest {
     @Mock
     private BoardRepository boardRepository;
@@ -33,12 +34,21 @@ public class BoardServiceTest extends AbstractServiceTest {
     @InjectMocks
     private BoardService boardService;
 
+    private Board testBoard;
+
+    @BeforeEach
+    void setUp() {
+        this.testBoard = Board.builder().id(TEST_FIRST_BOARD_ID).title(TEST_BOARD_TITLE).content(TEST_BOARD_CONTENT)
+                .product(TEST_PRODUCT).sellerAddress(TEST_SELLER_ADDRESS).buyer(TEST_SECOND_USER).seller(TEST_USER)
+                .salesStatus(SalesStatusType.SALE).build();
+    }
 
     @DisplayName("게시물이 생성되는지")
     @Test
+    @Order(1)
     public void create() {
         when(productService.findById(anyLong())).thenReturn(TEST_PRODUCT);
-        when(boardRepository.save(any())).thenReturn(TEST_BOARD);
+        when(boardRepository.save(any())).thenReturn(testBoard);
 
         CreateBoardResponseView board = boardService.create(CREATE_BOARD_REQUEST_VIEW, TEST_USER);
 
@@ -47,6 +57,7 @@ public class BoardServiceTest extends AbstractServiceTest {
 
     @DisplayName("게시물 리스트르 조회하는지")
     @Test
+    @Order(2)
     public void list() {
         when(boardRepository.findAll()).thenReturn(TEST_BOARDS);
 
@@ -57,8 +68,9 @@ public class BoardServiceTest extends AbstractServiceTest {
 
     @DisplayName("특정 게시물을 ID 로 조회 가능한지")
     @Test
+    @Order(3)
     public void findById() {
-        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BOARD));
+        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(testBoard));
 
         FindBoardResponseView board = boardService.retrieve(DEFAULT_ID);
 
@@ -70,6 +82,7 @@ public class BoardServiceTest extends AbstractServiceTest {
 
     @DisplayName("게시물 상세 조회 시 존재하지 않는 경우 예외를 던지는지")
     @Test
+    @Order(4)
     public void findByIdFail() {
         when(boardRepository.findById(any())).thenThrow(IllegalArgumentException.class);
 
@@ -78,18 +91,9 @@ public class BoardServiceTest extends AbstractServiceTest {
         });
     }
 
-    @DisplayName("게시물 수정이 가능한지")
-    @Test
-    public void modify() {
-        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BOARD));
-
-        Board modifiedBoard = boardService.modify(MODIFY_BOARD_REQUEST_DTO, DEFAULT_ID);
-
-        assertThat(modifiedBoard.getTitle()).isEqualTo(TEST_MODIFY_BOARD_TITLE);
-    }
-
     @DisplayName("카테고리 별 게시물 조회가 가능한지")
     @Test
+    @Order(5)
     public void listByCategory() {
         when(boardRepository.findAllByCategory(any())).thenReturn(LIST_BOARD_BY_CATEGORY_RESPONSE_VIEW);
 
@@ -100,8 +104,9 @@ public class BoardServiceTest extends AbstractServiceTest {
 
     @DisplayName("게시글 예약 신청 시 해당 게시글을 가져와 상태값을 예약으로 변경시키는지")
     @Test
+    @Order(6)
     public void reserve() {
-        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BOARD));
+        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(testBoard));
 
         Board board = boardService.reserve(MODIFY_BOARD_STATUS_REQUEST_VIEW, TEST_USER);
 
@@ -110,12 +115,23 @@ public class BoardServiceTest extends AbstractServiceTest {
 
     @DisplayName("게시글 판매 완료 시")
     @Test
+    @Order(7)
     public void complete() {
-        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(TEST_BOARD));
+        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(testBoard));
 
         Board board = boardService.reserve(MODIFY_BOARD_STATUS_REQUEST_VIEW, TEST_USER);
 
         assertThat(board.getSalesStatus()).isEqualTo(SalesStatusType.RESERVE);
     }
 
+    @DisplayName("게시물 수정이 가능한지")
+    @Test
+    @Order(8)
+    public void modify() {
+        when(boardRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(testBoard));
+
+        Board modifiedBoard = boardService.modify(MODIFY_BOARD_REQUEST_DTO, DEFAULT_ID);
+
+        assertThat(modifiedBoard.getTitle()).isEqualTo(TEST_MODIFY_BOARD_TITLE);
+    }
 }
