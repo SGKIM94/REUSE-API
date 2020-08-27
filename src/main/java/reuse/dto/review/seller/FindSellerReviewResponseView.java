c5eaaf7d463682d429a1e06cae78afcf94756a9e
sanggu <tkdrn8578@naver.com>
2020-08-27 22:58:51 +0900

    [MODIFY] seller 를 FindByIdResponseView 로 리턴하도록 수정

--- src/main/java/reuse/dto/review/seller/FindSellerReviewResponseView.java
+++ src/main/java/reuse/dto/review/seller/FindSellerReviewResponseView.java
@@ -5,20 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import reuse.domain.SellerReview;
-import reuse.domain.User;
+import reuse.dto.user.FindByIdResponseView;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class FindSellerReviewResponseView {
     private Long id;
-    private User seller;
+    private FindByIdResponseView seller;
     private Integer score;
     private String title;
     private String content;
 
     @Builder
-    public FindSellerReviewResponseView(Long id, User seller, Integer score, String title, String content) {
+    public FindSellerReviewResponseView(Long id, FindByIdResponseView seller, Integer score, String title, String content) {
         this.id = id;
         this.seller = seller;
         this.score = score;
@@ -33,7 +33,7 @@ public class FindSellerReviewResponseView {
 
         return FindSellerReviewResponseView.builder()
                 .id(sellerReview.getId())
-                .seller(sellerReview.getSeller())
+                .seller(FindByIdResponseView.toDto(sellerReview.getSeller()))
                 .score(sellerReview.getScore())
                 .title(sellerReview.getTitle())
                 .content(sellerReview.getContent())
