555349adddced63efcc214f98552e286ceab4347
sanggu <tkdrn8578@naver.com>
2020-09-11 19:41:23 +0900

    [MODIFY] chat room redis 작업을 테스트를 위해 제거함에 따른 제거

--- src/test/java/reuse/repository/ChatRoomRepositoryTest.java
+++ src/test/java/reuse/repository/ChatRoomRepositoryTest.java
@@ -8,21 +8,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reuse.chat.repository.ChatRoomRepository;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 public class ChatRoomRepositoryTest {
-    @Autowired
-    private ChatRoomRepository chatRoomRepository;
-
-    @DisplayName("채팅 방을 REDIS 로 정상적으로 생성되는지")
-    @Test
-    public void createChatRoom() {
-        //given
-
-
-        //when
-
-        //then
-        assertThat().isEqualTo();
-    }
 }
