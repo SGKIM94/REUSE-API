ceb9070ac41c28fa6d674bbd9c95b2a98df88545
sanggu <tkdrn8578@naver.com>
2020-09-15 23:46:57 +0900

    [ADD] UserHandlerMethodArgumentResolver 에서 resolveArgument 로 사용자 정보를 가져오는지 test code 추가

---
+++ src/test/java/reuse/security/UserHandlerMethodArgumentResolverTest.java
@@ -0,0 +1,52 @@
+package reuse.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.context.request.NativeWebRequest;
+import reuse.domain.User;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static reuse.fixture.UserFixture.TEST_USER;
+import static reuse.security.JwtAuthInterceptor.AUTH_USER_KEY;
+
+@ExtendWith(MockitoExtension.class)
+public class UserHandlerMethodArgumentResolverTest {
+    @Mock
+    private MethodParameter parameter;
+
+    @Mock
+    private NativeWebRequest request;
+
+    @Mock
+    private LoginUser annotedLoginUser;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    private UserHandlerMethodArgumentResolver userHandlerMethodArgumentResolver;
+
+    @BeforeEach
+    public void setup() {
+
+        userHandlerMethodArgumentResolver = new UserHandlerMethodArgumentResolver();
+    }
+
+    @Test
+    public void 사용자정보가_파라미터로_넘어가_정상처리되는경우() {
+        when(parameter.getParameterAnnotation(LoginUser.class)).thenReturn(annotedLoginUser);
+        when(request.getNativeRequest()).thenReturn(httpServletRequest);
+        when(httpServletRequest.getAttribute(AUTH_USER_KEY)).thenReturn(TEST_USER);
+
+        User loginUser
+                = (User) userHandlerMethodArgumentResolver.resolveArgument(parameter, null, request, null);
+
+        assertThat(loginUser).isEqualTo(TEST_USER);
+    }
+}
