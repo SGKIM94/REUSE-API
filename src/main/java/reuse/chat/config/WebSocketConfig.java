78becafcfdf834326467d092dcac30d5b6b358b6
kimsanggu <tkdrn8578@paymint.co.kr>
2020-09-22 18:35:42 +0900

    [MODIFY] websocket endponint 를 connect 로 변경

--- src/main/java/reuse/chat/config/WebSocketConfig.java
+++ src/main/java/reuse/chat/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chats").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/connect").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
