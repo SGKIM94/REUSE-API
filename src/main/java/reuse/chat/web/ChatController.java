bd4099f18a62177807ee7c62e6cfde21b0b9a19e
kimsanggu <tkdrn8578@paymint.co.kr>
2020-09-22 18:35:10 +0900

    [MODIFY] websocket 의 endpoint 를 구분하기 위해서 분리작업
     * websocket connect 와 http get method 와 혼동하는 현상으로 오류 발생

--- src/main/java/reuse/chat/web/ChatController.java
+++ src/main/java/reuse/chat/web/ChatController.java
@@ -3,12 +3,8 @@ package reuse.chat.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
-import reuse.chat.domain.ChatMessage;
 import reuse.chat.dto.CreateChatRequestView;
-import reuse.chat.service.ChatMessageService;
 import reuse.chat.service.ChatRoomService;
 import reuse.domain.User;
 import reuse.security.LoginUser;
@@ -18,11 +14,9 @@ import reuse.security.LoginUser;
 @RequestMapping("/chats")
 public class ChatController {
     private final ChatRoomService chatRoomService;
-    private final ChatMessageService chatMessageService;
 
-    public ChatController(ChatRoomService chatRoomService, ChatMessageService chatMessageService) {
+    public ChatController(ChatRoomService chatRoomService) {
         this.chatRoomService = chatRoomService;
-        this.chatMessageService = chatMessageService;
     }
 
     @PostMapping
@@ -38,15 +32,5 @@ public class ChatController {
     @GetMapping()
     public ResponseEntity list() {
         return ResponseEntity.ok().body(chatRoomService.findAll());
-    }
-
-    @MessageMapping("/message")
-    @SendTo("/pub/message")
-    public ResponseEntity message(ChatMessage message) {
-        log.info("메시지를 보냈습니다.");
-        log.info("message: " + message);
-
-        chatMessageService.publishMessage(message);
-        return ResponseEntity.ok().build();
     }
 }
