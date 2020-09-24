eaa737a006407d27be01dd7ce0c466f8d1507cb0
sanggu <tkdrn8578@naver.com>
2020-09-24 23:52:26 +0900

    [ADD] chat message 를 저장하는 service code 추가

--- src/main/java/reuse/chat/service/ChatMessageService.java
+++ src/main/java/reuse/chat/service/ChatMessageService.java
@@ -3,14 +3,18 @@ package reuse.chat.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
+import reuse.chat.domain.ChatMessage;
 import reuse.chat.dto.PublishChatRequestView;
+import reuse.chat.repository.ChatMessageRepository;
 
 @Service
 @Slf4j
 public class ChatMessageService {
     private final SimpMessageSendingOperations messageSendingOperations;
+    private final ChatMessageRepository chatMessageRepository;
 
-    public ChatMessageService(SimpMessageSendingOperations messageSendingOperations) {
+    public ChatMessageService(SimpMessageSendingOperations messageSendingOperations, ChatMessageRepository chatMessageRepository) {
+        this.chatMessageRepository = chatMessageRepository;
         this.messageSendingOperations = messageSendingOperations;
     }
 
@@ -21,6 +25,12 @@ public class ChatMessageService {
 
         log.info("메시지가 들어왔습니다.");
 
+
+        //TODO: 채팅 메시지가 들어오면 저장하고 해당 방에 있는 모든 message 를 리턴하도록 수정
         messageSendingOperations.convertAndSend("/sub/chats/" + message.getRoomId(), message);
+    }
+
+    public ChatMessage create(ChatMessage chat) {
+        return chatMessageRepository.save(chat);
     }
 }
