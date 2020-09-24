e8d38165ec584d4c730c09b8cb9127168e0eca66
sanggu <tkdrn8578@naver.com>
2020-09-24 23:48:30 +0900

    [ADD] chat message 가 보내졌을 때 DB 에 저정하기 위한 TEST CODE 추가

--- src/test/java/reuse/chat/service/ChatMessageServiceTest.java
+++ src/test/java/reuse/chat/service/ChatMessageServiceTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reuse.chat.repository.ChatMessageRepository;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static reuse.chat.fixture.ChatFixture.TEST_CHAT_MESSAGE_DTO;
+import static reuse.chat.fixture.ChatFixture.*;
 
 @ExtendWith(SpringExtension.class)
 public class ChatMessageServiceTest {
@@ -19,9 +20,12 @@ public class ChatMessageServiceTest {
     @MockBean
     private SimpMessageSendingOperations messageSendingOperations;
 
+    @MockBean
+    private ChatMessageRepository chatMessageRepository;
+
     @BeforeEach
     void setUp() {
-        chatMessageService = new ChatMessageService(messageSendingOperations);
+        chatMessageService = new ChatMessageService(messageSendingOperations, chatMessageRepository);
     }
 
     @DisplayName("입장 시 입장 메시지가 발송되는지")
@@ -38,5 +42,15 @@ public class ChatMessageServiceTest {
 
         //then
         verify(messageSendingOperations).convertAndSend(any(), (Object) any());
+    }
+
+    @DisplayName("채팅 메시지가 DB 에 저장되는지")
+    @Test
+    public void create() {
+        //when
+        chatMessageService.create(TEST_CHAT_MESSAGE_DTO);
+
+        //then
+        verify(chatMessageRepository).save(TEST_CHAT_MESSAGE);
     }
 }
