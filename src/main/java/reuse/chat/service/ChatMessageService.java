ea74854ce627aa408971ab6bf019fa8b0baa656f
sanggu <tkdrn8578@naver.com>
2020-09-26 23:58:13 +0900

    [ADD] publishMessage 에 sender 를 받아 해당 room 에 대한 정보 조회 후 message 에 저장하도록 추갸

--- src/main/java/reuse/chat/service/ChatMessageService.java
+++ src/main/java/reuse/chat/service/ChatMessageService.java
@@ -4,29 +4,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import reuse.chat.domain.ChatMessage;
+import reuse.chat.domain.ChatRoom;
 import reuse.chat.dto.PublishChatRequestView;
 import reuse.chat.repository.ChatMessageRepository;
+import reuse.domain.User;
 
 @Service
 @Slf4j
 public class ChatMessageService {
     private final SimpMessageSendingOperations messageSendingOperations;
     private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomService chatRoomService;
 
-    public ChatMessageService(SimpMessageSendingOperations messageSendingOperations, ChatMessageRepository chatMessageRepository) {
+    public ChatMessageService(SimpMessageSendingOperations messageSendingOperations, ChatMessageRepository chatMessageRepository, ChatRoomService chatRoomService) {
         this.chatMessageRepository = chatMessageRepository;
         this.messageSendingOperations = messageSendingOperations;
+        this.chatRoomService = chatRoomService;
     }
 
-    public void publishMessage(PublishChatRequestView message) {
+    public void publishMessage(PublishChatRequestView message, User sender) {
         if (message.isJoinMessageType()) {
             message.publishJoinMessage();
         }
 
         log.info("메시지가 들어왔습니다.");
 
+        ChatRoom chatRoom = chatRoomService.findById(message.getRoomId());
+        create(message.toEntity(chatRoom, sender));
 
-        //TODO: 채팅 메시지가 들어오면 저장하고 해당 방에 있는 모든 message 를 리턴하도록 수정
         messageSendingOperations.convertAndSend("/sub/chats/" + message.getRoomId(), message);
     }
 
