dc9f5926bb0bda193fc528856ee88b6a26a08d65
sanggu <tkdrn8578@naver.com>
2020-10-01 19:58:19 +0900

    [ADD] repository 에 chatRoomId 로 해당 room 에 존재하는 모든 chatMessage 를 가져오는 query 추가

--- src/main/java/reuse/chat/repository/ChatMessageRepository.java
+++ src/main/java/reuse/chat/repository/ChatMessageRepository.java
@@ -3,5 +3,8 @@ package reuse.chat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import reuse.chat.domain.ChatMessage;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByChatRoomId(Long roomId);
 }
