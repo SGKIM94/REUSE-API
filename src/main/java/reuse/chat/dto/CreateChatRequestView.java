65f2b436ea0c4cd1b205d51f08b5b15a6e54a073
sanggu <tkdrn8578@naver.com>
2020-10-08 23:04:52 +0900

    [ADD] createChatRequestView 의 name 에 필수 값 어노테이션 추가

--- src/main/java/reuse/chat/dto/CreateChatRequestView.java
+++ src/main/java/reuse/chat/dto/CreateChatRequestView.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class CreateChatRequestView {
+    @NotBlank(message = "채팅 방명은 필수 값입니다.")
     private String name;
 
     @Builder
