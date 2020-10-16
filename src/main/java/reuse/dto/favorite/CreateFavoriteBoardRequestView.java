82838238bc439aea5d0d7c1be497736740e26259
kimsanggu <tkdrn8578@paymint.co.kr>
2020-10-16 18:29:55 +0900

    [ADD] favorite board 생성 시 board ID 필수 값으로 추가

--- src/main/java/reuse/dto/favorite/CreateFavoriteBoardRequestView.java
+++ src/main/java/reuse/dto/favorite/CreateFavoriteBoardRequestView.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class CreateFavoriteBoardRequestView {
+    @NotBlank(message = "board ID 는 필수 값입니다.")
     private Long boardId;
 
     @Builder
