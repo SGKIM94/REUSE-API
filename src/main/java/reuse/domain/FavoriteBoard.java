f871553a6810c9acfe45ee256e7359a25993f399
sanggu <tkdrn8578@naver.com>
2020-10-30 23:54:37 +0900

    [MODIFY] favoriteBoard 는 user 에게 ManyToOne 이기 때문에 User 가 OneToMany 로 연결되어 양방향 바인딩하도록 수정

--- src/main/java/reuse/domain/FavoriteBoard.java
+++ src/main/java/reuse/domain/FavoriteBoard.java
@@ -3,16 +3,13 @@ package reuse.domain;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 
 @Entity
 @NoArgsConstructor
 public class FavoriteBoard extends AbstractEntity {
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
