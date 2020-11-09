8795601e9977d6d7c1bd0d0b13d40c80c2170cb0
sanggu <tkdrn8578@naver.com>
2020-11-09 23:02:11 +0900

    [ADD] image Entity 를 만드는 toEntity 에 대한 TEST CODE 추가

--- src/test/java/reuse/domain/ImageTest.java
+++ src/test/java/reuse/domain/ImageTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static reuse.fixture.ProductFixture.TEST_PRODUCT;
+import static reuse.fixture.ProductFixture.TEST_PRODUCT_NAME;
+import static reuse.fixture.ProductImagesFixture.FIRST_IMAGE_URL;
 import static reuse.fixture.ProductImagesFixture.TEST_IMAGE_URLS;
 
 public class ImageTest {
@@ -17,5 +19,15 @@ public class ImageTest {
         List<Image> images = Image.convertUrlsToImages(TEST_PRODUCT, TEST_IMAGE_URLS);
 
         assertThat(images.size()).isEqualTo(5);
+    }
+
+    @DisplayName("경로와 product 로 Image Entity 를 만들 수 있는지")
+    @Test
+    public void toEntity() {
+        //when
+        Image image = Image.toEntity(FIRST_IMAGE_URL, TEST_PRODUCT);
+
+        assertThat(image.getUrl()).isEqualTo(FIRST_IMAGE_URL);
+        assertThat(image.getProduct().getName()).isEqualTo(TEST_PRODUCT_NAME);
     }
 }
